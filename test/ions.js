640734d4a7e4043f11567b083bf02d34e06b7c28
Mike Lee <git@iskitz.com>
2016-11-22 22:01:09 -0800

    +/ Updated README's date+timestamps + ionify.node's face + added do@ionify , next.id@ionify + inital tests /

---
+++ test/ions.js
@@ -0,0 +1,16 @@
+;
+
++
+{ re:
+    { id: "ions@test.ionify.2016.11.22-08"
+    , is: "ionify's test ions"
+    , by:
+        [ {creator: "mike.lee@iskitz",  at: "2007.09-04",     in: "forest-hills.new-york.usa.earth"}
+        , {authors:     "team@ionify",  at: "2016.11.22-08",  in:   "san-jose.california.usa.earth"}
+        ]
+    },
+
+  get: ["tests"]
+}
+
+;
---
+++ test/tests.js
@@ -0,0 +1,112 @@
+;
+
+['• . •']
+
++
+{ re:
+    { id: "tests@ionify"
+    , it: "Tests ionify"
+    , by: "Mike.Lee@iskitz"
+    , at: "2016.11.13-08...2016.05.18-07"
+    , in: "san-jose.california.usa.earth"
+    }
+}
+
+
+                            var hey =
+{ on: "hi"
+, hi: function onHi (ion)
+        { +{log:"hi " + ion.hi + "!"}
+        }
+};
+
+           hey            +
+{hi:     "iamai"      }   +
+{hi: "ひろことマイケル"  }   +
+{hi:   "['• . •']"    }   +
+           hey            ;
+
+
+
++
+{ do: "hey"
+, hey:
+    function hi ()
+      { +{log: hi.this.do + " did log"}
+      }
+}
+
++
+{ get: "http://ionify.net/ions/test/log.js"
+, do:
+    function hi ()
+      { +{log: "did " + hi.this.get}
+      }
+}
+
++ {do: {log: "¡hey!  do  works!"}}
+
++ {do:"went", went:{log:'¡hey! "do" works!'}}
+
+
+; +[':'] ;    ///*
+
++
+{ re:
+    ["playing with syntax for getting non-ion scripts in order"],
+
+  do:
+    [ { get: "http://ajile.net/use/com.iskitz.ajile.js?mvcoff,mvcshareoff"
+      , now: true
+      },
+      { get: "http://ajile.net/play/api/scripts/com.iskitz.ajile.examples.LoadExample.js"
+      , now: true
+      ,  do: "doIt"
+      }
+    ],
+  doIt:
+      function onLoadExample ()
+      {  com.iskitz.ajile.examples.LoadExample()
+      }
+}
+
+; +{O:0} ;
+
++
+{ re:
+    ["playing with syntax for getting multiple non-ion scripts in order"],
+
+  get:
+    [ "http://ajile.net/use/com.iskitz.ajile.js?mvcoff,mvcshareoff"
+    , "http://ajile.net/play/api/scripts/com.iskitz.ajile.examples.LoadExample.js"
+    ],
+  now: true,
+  do:
+    [ ,
+      function onLoadExample ()
+      {  com.iskitz.ajile.examples.LoadExample()
+      }
+    ]
+}
+
+; +['-:-'] ;
+
++
+{ re:
+    ["playing with syntax for getting non-ion scripts in order"],
+
+  getoff:
+    [ { now: true, js: "http://ajile.net/use/com.iskitz.ajile.js?mvcoff,mvcshareoff"},
+      { now: true, js: "http://ajile.net/play/api/scripts/com.iskitz.ajile.examples.LoadExample.js"
+      , do: "doIt"
+      }
+    ],
+  now: true,
+  doIt:
+      function onLoadExample ()
+      {  com.iskitz.ajile.examples.LoadExample()
+      }
+}
+
+//*/
+;
