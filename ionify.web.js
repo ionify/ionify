cb7da3fed8842f7a2ea786c76c22a48c6f4f9dd1
Mike Lee <git@iskitz.com>
2017-03-21 06:30:00 -0700

    ~ [/~ion safer than +ion / ,/ +get.then <= -get.do; / ,/ ion.method.ion + ion.method.this = ion / ,/ ionPropertyStories = [/.../] for notes + todos / ,/ +on correctly adds observers / ,/ all th1s's <= ion.id / ,/ +['• . •'] anemoji activates ionify / ,/ +['- . •'] anemoji deactivates ionify / ,/ +['• . -'] anemoji deactivates ionify / ,/ +['- . -'] anemoji disables ionify /
    ,/ +debug temporarily disabled via +web /
    ,/ +info level logging /
    ,/ +error logs + throws new Error /
    ,/ +web.onerror tracks all errors /
    ]
    ;

--- ionify.web.js
+++ ionify.web.js
@@ -1,12 +1,10 @@
-;
-
-+
+~
 { re:
     { id: "web@ionify.0.1"
     , is: "web-based implicit object notation invented for you"
     , by:
         [ {creator: "mike.lee@iskitz", at: "2007.09-04"   }
-        , {authors:     "team@ionify", at: "2016.12.03-08"}
+        , {authors:     "team@ionify", at: "2017.03.21-07"}
         ],
 
       todo:
@@ -15,13 +13,43 @@
         ]
     },
 
+
+  on:
+    [ ["get", "then"  ]
+    , ["get", "use" ]
+    ,  "get"
+    ,  "debug", "error", "info", "log", "warn"
+    ],
+
+
   valueOf:
     function ionifyWeb ()
       {   delete this.valueOf
+      ;   this.track  ()
       ;   this.works  ()
       ;   this.locate ()
-      ;   this.get    ({get:"ionify@ionify", do:this})
+
+      ;   var initialized =
+            [ this
+            , {debug:!true}
+            , {log: "やた！アイオニファイログわばちり！"}
+            , {hosted:true}
+            ]
+
+      ;   this.get ({get: "ionify@ionify", then: [initialized]})
       },
+
+
+  track:
+    function track ()
+      { onerror =
+           function onUncaughtError (message, url, line, column, error)
+             { ~{warn : [message, error.stack]}
+               ~{debug: [message, error.stack, url, line, column, error]}
+               return true
+             }
+      },
+
 
   works:
     function works ()
@@ -31,29 +59,26 @@
       ;   return true
       },
 
+
   errors:
     { noAlert : new Error ("web@ionify.net needs the window.alert() API")
     , noDOM   : new Error ("web@ionify.net needs the DOM: Document Object Module API")
     },
 
+
   locate:
     function locate ()
-      { var script  =[  document.currentScript,,] || document.scripts
+      { var script  =[  document.currentScript ,,]
+                    ||  document.scripts
                     ||  document.head.getElementsByTagName ("script")
           , path    =   script && script [script.length - 2].src
           ; path    &&  (this.get$.PATH.ionify = path.replace (/(.+\/).+$/, "$1"))
       },
 
-  on:
-    [ ["get", "do"  ]
-    , ["get", "use" ]
-    ,  "get"
-    ,  "log", "debug", "error", "warn"
-    ],
 
-  getTodo:
+  getStories:
     [ /2016.11.16-08/
-    , /ionify: todos: Enable inline + referrable via ion.methodTodo:[storie(s)]/
+    , /ionify: todos: Enable inline + referrable via ion.methodStorie:[storie(s)]/
     , /ionify: todos: Disable eon during todo evaluations to allow sample code/
     , /   get: Handle URLs with existing file extension(s)/
     , /ionify: Enable ion.methods to define static method variables via method$/
@@ -64,12 +89,14 @@
                     { re: {id:"...", todo:["..."]}
                     , var1: 9
                     , var2: {}
-                    , do: ["function1", "act1"]
+                    , then: ["function1", "act1"]
                     , function1:function f1(){ f1.var1 + f1.var2 }
                     , act1:{log:"Convenient!"}
                     }
                 }
     ],
+
+
   get$:
     { HTTP: (/^\w+:\/\//)                       // matches URL protocols
     ,   ID: (/(?:(.*)@(\D*)|(\D*))(\d+.*)*/)    // matches ((api)@(host.) | (api.)) version#
@@ -84,21 +111,23 @@
     ,  EXT: (/(\.\D*$)/)    // matches file extensions
     ,  URL:
         function getURL (match, name, space, file, version, offset, string)
-          {   var th1s  = getURL.this
-          ,       ext   = th1s.EXT.exec (match)
-          ;   name      = name  && ( name.match (th1s.NAME) || [, name])[1]
-          ;   space     = space && (space.match (th1s.NAME) || [,space])[1]
-          ;   file      = file  && ( file.match (th1s.NAME) || [, file])[1]
+          {   var web   = getURL.this
+          ,       ext   = web.EXT.exec (match)
+          ;   name      = name  && ( name.match (web.NAME) || [, name])[1]
+          ;   space     = space && (space.match (web.NAME) || [,space])[1]
+          ;   file      = file  && ( file.match (web.NAME) || [, file])[1]
 
-          ;   return th1s.PATH [space] + (name || file) + (version || "")
-                                       + /*(ext ? ext [1] :*/(th1s.TYPE)
+          ;   return web.PATH [space] + (name || file) + (version || "")
+                                       + /*(ext ? ext [1] :*/(web.TYPE)
           }
     },
+
+
   get:
     function get (ion)                                                          // for +{get: "script.js"}
       { var get$          = (get.this || (get.this = this)).get$
           , url           = ion.get
-          , act           = ion.do
+          , act           = ion.then
           ; get$.URL.this = get$
 
           ; !get$.PATH           && (get$.PATH = this.path.ionify)
@@ -109,7 +138,9 @@
         for (var last=url.length, next=-1; ++next < last;)
           { function got ()
               {  var then = got.then
-              ;  typeof then === "string" ? +(ion [then]) : +then
+alert(["got",path])
+              ~  {debug: ["got ",path," doing ",then,"..."]}
+              ;  typeof then === "string" ? ~ion[then] : ~then
               }
 
             var script  = document.createElement ("script")
@@ -122,14 +153,17 @@
               ; script.async  = !(ion.now === true)
               ; script.src    = path
               ; document.head.appendChild (script)
-              ; +{debug:"getting: "+path}
+alert(["get",path])
+              ~ {debug: ["get",path,"..."]}
           }
       },
 
-  "get do":
-    function getDo (ion)                                                       // for +{get: "script.js", do: "action"}
+
+  "get then":
+    function getDo (ion)                                                       // for +{get: "script.js", then: "action"}
       { return getDo.this.get (ion)
       },
+
 
   "get use":
     function getUse (ion)                                                      // for +{get: "script.js", use: "member"}
@@ -142,9 +176,10 @@
 
       ; var uses = ion.use
       ; !Array.isArray (uses) && (uses = [uses])
-      ; +{on:uses, do:use};
+      ~ {on:uses, do:use}
       ; return getUse.this.get (ion)
       },
+
 
   debug:
     function debug (ion)
@@ -153,12 +188,23 @@
       ; debug.this.log (ion)
       },
 
+
   error:
-    function error (ion)
+    function logError (ion)
       { ion.as  = "error"
       ; ion.log = ion.error
-      ; throw ion.error
+      ; logError.this.log (ion)
+      ~ new Error (ion.error)
       },
+
+
+  info:
+    function logInfo (ion)
+      { ion.as  = "info"
+      ; ion.log = ion.info
+      ; logInfo.this.log (ion)
+      },
+
 
   log:
     function log (ion)
@@ -185,30 +231,29 @@
           }
 
         function sense (ion)
-          { id         = (ion.from || th1s.re.id) + ": "
-          ; level      = ion.as    || "log"
+          { id         = (ion.re.from || web.re.id) + ": "
+          ; level      = ion.as       || "log"
           ; ("boolean" == typeof ion [level]) && (sense [level] = ion [level])
           ; return sense [level]
           }
 
         var id
           , level
-          , th1s        = log.this
+          , web         = log.this
           , iOSPath     = (/^file:\/\/.*\/var\/mobile\//)
           , noConsole   = document.URL.match (iOSPath)
-          ; sense.debug = false
+          ; sense.debug = true
           ; sense.error = true
           ; sense.log   = true
           ; sense.warn  = true
-          ; (th1s.log   = noConsole ? popup : cons0le) (ion)
+          ; (web.log    = noConsole ? popup : cons0le) (ion)
       },
 
+
   warn:
-    function warn (ion)
-      { ion.as = "warn"
+    function logWarn (ion)
+      { ion.as  = "warn"
       ; ion.log = ion.warn
-      ; warn.this.log (ion)
+      ; logWarn.this.log (ion)
       }
 } //+web@ionify
-
-;
--- test/ions.js
+++ test/ions.js
@@ -2,15 +2,16 @@
 
 +
 { re:
-    { id: "ions@test.ionify.2016.11.22-08"
+    { id: "ions@test.ionify.2017.03.20-07"
     , is: "ionify's test ions"
     , by:
         [ {creator: "mike.lee@iskitz",  at: "2007.09-04",     in: "forest-hills.new-york.usa.earth"}
-        , {authors:     "team@ionify",  at: "2016.11.22-08",  in:   "san-jose.california.usa.earth"}
+        , {authors:     "team@ionify",  at: "2017.03.20-07",  in:   "san-jose.california.usa.earth"}
         ]
-    },
+    }
 
-  get: ["tests"]
+,  debug: !true
+,  get  : ["tests"]
 }
 
 ;
