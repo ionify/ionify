6a36f7b44fe4233f5e398b97c8229e0b9af33384
Mike Lee <git@iskitz.com>
2017-04-02 22:55:10 -0700

    ~/ New directory structure: ids: id confirmation, info: docs + etc., ions: ionify's ions + tests: all tests /
    +/ Experimenting with id ion's structure + content via ids/mike.lee /

---
+++ ids/mike.lee.js
@@ -0,0 +1,25 @@
+;
+
+~
+{ id: "mike.lee@iskitz"
+, is: "Michael Lee",
+
+  creator:
+    [ "ionify", "ajile" ],
+
+  author:
+    [ "wakatta", "jeni", "anemoji", "webOS Calendar", "webOS Calendar Fixes" ],
+
+  from:
+    [         "san-jose.california.usa.earth"
+    ,       "forest-hills.new-york.usa.earth"
+    , "georgetown.guyana.south-america.earth"
+    ],
+
+  github  : "iskitz",
+  twitter : "iskitz",
+  linkedin: "mikeleepro",
+  web     : "mike.iskitz.net"
+}
+
+;
