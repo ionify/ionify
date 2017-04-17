a77aaab424db5bf224e95e64a64eaff04c7ed540
Mike Lee <git@iskitz.com>
2017-04-17 02:33:30 -0700

    ~/code cleanup + new to dos/

--- web.js
+++ web.js
@@ -4,33 +4,37 @@
 +['0 . 0']+
      -
 
-{ re:
-    { id: "web@ionify.0.1"
-    , is: "implicit object notation invented for your web"
-    , by:
-        [ {creator: "mike.lee@iskitz", at: "2007.09-04"   }
-        , {authors:     "team@ionify", at: "2017.04.04-07"}
-        ],
+/web@ionify activating.../
 
-      todo:
-        [ "on: Update ionify.js to handle action grammar"
-        , "qa: Add tests for web@ionify + its actions"
++
+{ re:
+    { id:  "web.0.1@ionify"
+    , is:  "implicit object notation invented for your web"
+    , by: ["mike.lee@ionify", "team@ionify"]
+    , at:  "2017.04.17-07...2007.09-04"
+        ,
+      stories:
+        [ /note: .../
+        , /todo: Add tests for web@ionify + its actions/
+        , /todo: .../
         ]
     },
 
 
-  debug: false,
-    log: "やた！アイオニファイウェブログわばちり！"
+  debug
+    : false
     ,
+
 
   on:
     [ ["get", "then"]
-    , ["get", "use" ]
     ,  "get"
     ],
 
 
-  hosted: true,
+  hosted
+    : true
+    ,
 
 
   valueOf:
@@ -41,7 +45,7 @@
       ;   web.watch  ()
       ;   web.works  ()
       ;   web.locate ()
-      ;   web.get    ({get:["on@ionify", "web.log@ionify"], then:web})
+      ;   web.get    ({get:"on@ionify", then:web})
       },
 
 
@@ -50,7 +54,7 @@
       { onerror =
            function onUncaughtError (message, url, line, column, error)
              { ~{warn : [message, error.stack]}
-               ~{debug: [message, error.stack, url, line, column, error]}
+               ~{debug: [message, "errorstack", url, line, column, error]}
                return true
              }
       },
@@ -59,21 +63,21 @@
   works:
     function works ()
       {   var error = this.errors
-      ;   if (typeof document == "undefined") throw error.noDOM
+      ;   if (typeof document == "undefined") throw new Error (error.noDOM)
       ;   return true
       },
 
 
   errors:
-    { noDOM: new Error ("web@ionify.net needs the DOM: Document Object Module API")
+    { noDOM: "web@ionify needs the DOM: Document Object Module API"
     },
 
 
   locateStories:
-    [ " todo: Sense /ions/ path, don't hardcode it "
-    , / ... /
-    ],
-
+    [ "todo: Sense /ions/ path, don't hardcode it"
+    , /.../
+    ]
+    ,
   locate:
     function locate ()
       { var script  =[  document.currentScript ,,]
@@ -85,29 +89,15 @@
 
 
   getStories:
-    [ / 2017.04.05-07 /
-    , / todo: Update get$.PATH with @domains + their paths /
-
-    , /2016.11.16-08/
-    , /ionify: todos: Enable inline + referrable via ion.methodStorie:[storie(s)]/
-    , /ionify: todos: Disable eon during todo evaluations to allow sample code/
-    , /   get: Handle URLs with existing file extension(s)/
-    , /ionify: Enable ion.methods to define static method variables via method$/
-    , /ionify: ion.method: Auto-set .method$ members on ion while it evaluates/
-    , /ionify: Enhanced methods with static variables:/
-                +
-                { get:
-                    { re  : {id:"...", todo:["..."]}
-                    , var1: 9
-                    , var2: {}
-                    , do  : ["function1", "act1"]
-                    , function1:function f1(){ f1.var1 + f1.var2 }
-                    , act1:{log:"Convenient!"}
-                    }
-                }
-    ],
-
-
+    [ 'note: +{get: "ion.id" || "./script.js"}'
+    , /todo: Keep updating get$.PATH with @domains + their paths/
+    , /todo: Handle URLs with existing file extension(s)/
+    , /todo: Move got() + .then code to +get.then/
+    , "todo: Sense ion ids vs. script paths: ./script.js"
+    , /todo: Use +method@ionify to define +get/
+    , /todo: .../
+    ]
+    ,
   get$:
     { HTTP: (/^\w+:\/\//)                       // matches URL protocols
     ,   ID: (/(?:(.*)@(\D*)|(\D*))(\d+.*)*/)    // matches ((api)@(host.) | (api.)) version#
@@ -122,73 +112,64 @@
     ,  EXT: (/(\.\D*$)/)    // matches file extensions
     ,  URL:
         function getURL (match, name, space, file, version, offset, string)
-          {   var web   = getURL.this
-          ,       ext   = web.EXT.exec (match)
-          ;   name      = name  && ( name.match (web.NAME) || [, name])[1]
-          ;   space     = space && (space.match (web.NAME) || [,space])[1]
-          ;   file      = file  && ( file.match (web.NAME) || [, file])[1]
+          {   var get$  = getURL.this
+          ,       ext   = get$.EXT.exec (match)
+          ;       name  = name  && ( name.match (get$.NAME) || [, name])[1]
+          ;       space = space && (space.match (get$.NAME) || [,space])[1]
+          ;       file  = file  && ( file.match (get$.NAME) || [, file])[1]
 
-          ;   return web.PATH [space] + (name || file) + (version || "")
-                                      + /*(ext ? ext [1] :*/(web.TYPE)
+          ;   return get$.PATH [space] + (name || file) + (version || "")
+                                       + /*(ext ? ext [1] :*/(get$.TYPE)
           }
-    },
-
-
+    }
+    ,
   get:
-    function get (ion)                                                          // for +{get: "script.js"}
+    function get (ion)
       { var get$          = (get.this || (get.this = this)).get$
           , url           = ion.get
           , act           = ion.then
           ; get$.URL.this = get$
-
-          ; !get$.PATH           && (get$.PATH = this.path.ionify)
-          ; !Array.isArray (url) && (      url = [url])
-          ; !Array.isArray (act) && (      act = [act])
+          ; get$.PATH           || (get$.PATH = this.path.ionify)
+          ; Array.isArray (url) || (      url = [url])
+          ; Array.isArray (act) || (      act = [act])
           ;
 
         for (var last=url.length, next=-1; ++next < last;)
           { function got ()
               {  var then = got.then
-              ~  {debug: ["got ",path," doing ",then,"..."]}
-              ;  typeof then === "string" ? ~ion[then] : ~then
+              ~  {debug: ["got ",got.path," doing ",then,"..."]}
+              ;  typeof then === "string" ? +ion[then] : +then
               }
 
             var script  = document.createElement ("script")
               , then    = act [next] //|| (((then = {})[url [next]] = ion), then)
+              
               , path    = url [next].match   (get$.HTTP)
                         ? url [next]
                         : url [next].replace (get$.ID, get$.URL)
+
               ; then && (got.then = then) && (script.onload = got)
               ; script.type   = "text/javascript"
-              ; script.async  = !(ion.now === true)
-              ; script.src    = path
+              ; script.async  = ion.now !== true
+              ; script.src    = got.path  = path
               ; document.head.appendChild (script)
               ~ {debug: ["get",path,"..."]}
           }
       },
 
 
+  "get then stories":
+    [ 'note: +{get: ["ion.id" || "./script.js"], then: ["actions"]}'
+    , /todo: Move +get's code here + update +get to use this/
+    , /todo: Do +on:ion.id; faster than .onload/
+    ]
+    ,
   "get then":
-    function getThen (ion)                                                       // for +{get: "script.js", then: "action"}
+    function getThen (ion)
       { return getThen.this.get (ion)
-      },
-
-
-  "get use":
-    function getUse (ion)                                                      // for +{get: "script.js", use: "member"}
-      { function use (from)
-          { for (var thing, u=uses.length; u--;)
-              { thing       = uses [u]
-              ; ion [thing] = from [thing]
-              }
-          }
-
-      ; var uses = ion.use
-      ; !Array.isArray (uses) && (uses = [uses])
-      ~ {on:uses, do:use}
-      ; return getUse.this.get (ion)
       }
+}
 
-} //+web@ionify
-
++
+/web@ionify activated!/
 ;
