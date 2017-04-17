27c03f0f350d7f20d62be25a07f2d77706384651
Mike Lee <git@iskitz.com>
2017-04-17 03:30:32 -0700

    ~/created onType for own ion sensors, added logging, updated todos + cleaned code/

--- ions/on.js
+++ ions/on.js
@@ -4,73 +4,67 @@
 +['0 . 0']+
      -
 
++
+/on@ionify activating.../
++
+
 {  re:
     { id: "ionify.0.1"
     , is: "implicit object notation invented for you"
 
-    , by:
+        ,
+      by:
         [ {creator: "mike.lee@iskitz", at: "2007.09-04"   }
-        , {authors:     "team@ionify", at: "2017.04.04-07"}
+        , {authors:     "team@ionify", at: "2017.04.13-07"}
         ]
 
-    , stories:
-        [/note: .../
-        ,/todo: .resolve(): resolve names to ions: ionified references, e.g. functions/
+        ,
+      stories:
+        [ /note: .../
+        , /todo: Move .onAEON to on.aeon@ionify/
+        , /todo: Make .resolve to convert ids to ions: e.g. ionified references/
+        , /todo: Make a way to undo all +on:Type's/
         ]
-    },
+    }
 
 
-  ionifiedStories:
-    [/ todo: Auto-update as types are observed | ignored       /
-    ,/ todo: Enable +{ionified: typeof thing} = 1:true|0:false /
-    ,/ note: Use with typeof atom + atom.constructor.name      /
-    ],
-
-  ionified:
-    { Array    :  true
-    , boolean  : !true
-    , Error    :  true
-    , function :  true
-    , Function :  true
-    , number   : !true
-    , object   :  true
-    , Object   :  true
-    , RegExp   :  true
-    , string   : !true
-    , undefined: !true
-    },
-
-
+    ,
   valueOfStories:
- 	[/ todo: Move *.prototype.valueOf = this.on* to sensors ion /
-	,/ todo: Sense "this.ion" + "+ionId" + set to ion reference /
-	,/ note: ... /
-	],
-
+    [ 'todo: Sense "this.*" + "+*" + set to ion reference'
+    , /note: .../
+    ]
+    ,
   valueOf:
     function ionify ()
-      { Object  .prototype.valueOf = this.onObject
-      ; Function.prototype.valueOf = this.onFunction
-      ; Array   .prototype.valueOf = this.onArray
-      ; Error   .prototype.valueOf = this.onError
-    //; RegExp  .prototype.valueOf = this.onStorie
-      ; this.link ()
+      { this.link   ()
+      ; this.onType ({on: Object, Object: this.onION })
+      ; this.onType ({on: Array , Array : this.onAEON})
 
-      ; var initialize = {get: ["next.id@ionify", "do@ionify", "ions"]}
+        var initialize =
+              { get:
+                  [   "errors@ionify",   "on.storie@ionify"
+                  ,  "next.id@ionify",     "web.log@ionify"
+                  , "on.error@ionify", "on.function@ionify"
+                  ,  "on.aeon@ionify",          "do@ionify"
+                  ,      "use@ionify",               "ions"
+                  ]
+              }
+
       ; initialize.on  = "hosted"
       ; initialize.no  =  initialize
 
       ~ {on:"hosted", hosted:initialize}
 
-        return ""
-      },
+        return true
+      }
 
 
+    ,
   linkStories:
-    [/ todo: ion.this => ion.ionId + ion.ion /
-    ,/ todo: enable +{link: atom, to:thing}  /
-    ],
-
+    [ /todo: ion.this => ion.ionId + ion.ion/
+    , /todo: enable +{link:ion, to:thing}/
+    ]
+    ,
   link:
     function link (ion)
       { ion || (ion = link.this) || (link == this.link) && (ion = this);
@@ -85,20 +79,21 @@
           { thing = ion [property]
           ; if (!thing || typeof thing != "function") continue
           ; if (!ion.hasOwnProperty (property))       continue
-          ; /* thing [id] = */ thing.this = thing.use = thing.ion = ion
+          ; /* thing [id] = */ thing.this = thing.ion = ion
 
         //;(id!="this")&&alert("linking "+id+"."+property);
         //;(id != "this") && +{debug:"linked "+id+'.'+property}
           }
 
         return true
-      },
+      }
 
 
+    ,
   unlinkStories:
-    [/ todo: enable +{unlink: atom, from: thing} /
-    ],
-
+    [ /todo: enable +{unlink: ion, from: thing}/
+    ]
+    ,
   unlink:
     function unlink (ion)
       { ion || (unlink == this.unlink) && (ion = this);
@@ -112,20 +107,21 @@
           {  thing = ion [property]
           ;  (typeof thing == "function") && (thing [id] == ion) && (delete thing [id])
           }
-      },
+      }
 
 
+    ,
   senseStories:
-    [/ todo: no: Fix + re-enable /
-    ],
-
-  sense :
+    [ /todo: +no: Fix + re-enable/
+    ]
+    ,
+  sense:
     { '0 . 0'  : "activate"
     , '0 . -'  : "disable"
     , '- . 0'  : "disable"
     , '- . -'  : "deactivate"
   //, ionified : "ionified"
-  //, "on do"  : "on"
+  //, "on do"  : "on"         // see web.get.use@ionify
     ,  on      : "on"
   //,  no      : "no"
     },
@@ -135,17 +131,14 @@
     function activate (ion)
       {  if (ion [0] !== '0 . 0') return
       ;  var ionify = activate.this
-      ;  ionify.senses && (ionify.senses != ionify.sense) && +ionify
+      ;  ionify.senses && (ionify.senses != ionify.sense) && ~ionify
       },
 
 
   deactivate:
     function deactivate (ion)
-      {  delete Object  .prototype.valueOf
-      ;  delete Function.prototype.valueOf
-      ;  delete Array   .prototype.valueOf
-      ;  delete Error   .prototype.valueOf
-    //;  delete RegExp  .prototype.valueOf
+      {  delete Array .prototype.valueOf
+      ;  delete Object.prototype.valueOf
       ;  deactivate.this.unlink()
       },
 
@@ -161,18 +154,19 @@
   id:
     function getId (ion)
       { var    id =   ion.re ? ion.re.id : (ion.re = {id: ion.id}).id
-      ; return id || (ion.re.id = "sensor."
+      ; return id || (ion.re.id = "ion."
                                 + (getId.nextId ? ++getId.nextId
                                                 :  (getId.nextId = 1)))
-      },
+      }
 
 
+    ,
   noStories:
- 	[/ 133: resolve +{no:"this.this"}                    /
-	,/ 134: why check nion.on instead of nion.no?        /
-	,/ 137: future bug if multiple actions for same term /
-	],
-
+    [ /133: resolve +{no:"this.this"}?                   /
+    , /134: why check nion.on instead of nion.no?        /
+    , /137: future bug if multiple actions for same term /
+    ]
+    ,
   no:
     function no (ion)
       { if (!ion.no && !("no" in ion)) return ion;
@@ -195,14 +189,21 @@
           ; act   = sense [term]
           ; act  && act.ion && (id && id == act.ion.re.id) && delete sense [term]
           }
-      },
+      }
 
+
+    ,
   on:
     function on (ion)
       { if (!ion || !ion.on && !("on" in ion)) return ion
 
-         (ion.re && ion.re.id) || +{next:"sensor", id:ion};
-        !(ion.re && ion.re.id) && on.this.id (ion);
+      ; (ion.re && ion.re.id) || ~{next:"ion", id:ion}
+      ; (ion.re && ion.re.id) || on.this.id (ion)
+
+      ~ {debug: [ion.re.id,"on:",ion.on,JSON.stringify(ion.on)]}
+
+      ; if ("function" == typeof ion.on) return on.ion.onType (ion)
+      ;
 
         var grammars  = ion.on
           ; !Array.isArray (grammars) && (grammars = [grammars])
@@ -216,7 +217,8 @@
           , ionify = on.this
           , sense  = ionify.sense
           , id     = ion.re.id
-          ;
+          ~ {debug: Object.keys (sense)}
+
         while (++next < last)
           { grammar = grammars [next]
           ; Array.isArray (grammar) && (grammar = grammar.join (" "))
@@ -225,19 +227,32 @@
           ~ {debug:["knows?",id, grammar, grammar in sense]}
           }
 
-        ~ {debug: [id, grammars]}
+        ~{debug: [id, grammars]}
         return ion
-      },
+      }
 
 
+    ,
+  ionifiedStories:
+    [ /note: Use with typeof ion + ion.constructor.name/
+    , /todo: Auto-update as types are deactivated/
+    , /todo: Enable +{ionified: typeof thing} = 1:true|0:false/
+    ]
+    ,
+  ionified:
+    {
+    }
+
+
+    ,
   aesopStories:
-    [/ note: aesop: array-embedded sentence or phrase                       /
-    ,/ todo: find known words in each sentence                              /
-    ,/ todo: interpret via sentence(s), paragraph(s), chapter(s) + book(s)  /
-    ,/ todo: handle ['0 . 0'] via sense [thing] && sense [thing] (thing)    /
-    ,/ todo: use tbd name-to-ion resolver                                   /
-    ],
-
+    [ /note: aesop: array-embedded storie or phrase/
+    , /todo: find known words in each sentence/
+    , /todo: interpret via sentence(s), paragraph(s), chapter(s) + book(s)/
+    , /todo: handle ['0 . 0'] via sense [thing] && sense [thing] (thing)/
+    , /todo: use tbd name-to-ion resolver/
+    ]
+    ,
   aesop:
     function aesop (ion)
       { ion || (aesop == this.aesop) && (ion = this)
@@ -252,10 +267,11 @@
 
       ; !ionified [typeof sense] && (sense = ionify [sense])
       ; typeof sense == "function" ? sense (ion) : ~sense
-      },
+      }
 
 
-  onArray:
+    ,
+  onAEON:
     function onArray (ion)
       { ion || (ion = this)
 
@@ -276,32 +292,19 @@
           }
 
         return this
-      },
+      }
 
 
-  onError:
-    function onError (ion)
-      {  throw ion || this
-      },
-
-
-  onFunction:
-    function onFunction (ion)
-      {  ion || (ion = this)
-      ;  var got = ion.apply (this)
-      ~  {debug: ['+', ion.name || "anonymous", '() : ', got]}
-      ;  return got
-      },
-
-
-  onObjectStories:
-    [/ 221: sense => ArrayMap to preserve order + fast lookup.  /
-    ,/ 235: log all matched actions + their results.            /
-    ,/ ...: disable activated words + enable after all complete /
-    ,/ !!!: loop through ion's terms instead of known?          /
-    ],
-
-  onObject:
+    ,
+  onIONStories:
+    [ /todo: sense => ArrayMap to preserve order + fast lookup./
+    , /todo: log all matched actions + their results?          /
+    , /todo: disable activated words, enable after all matches /
+    , /todo: loop through ion's terms instead of known?        /
+    , /todo: Ignore similar actions after match: +get +get.then/
+    ]
+    ,
+  onION:
     function onObject (ion)
       { ion || (ion = this);
 
@@ -311,10 +314,11 @@
           , debug    = []
           , grammar , terms, term
           , next    , last
-          ;
 
-        onObject .ion  .id (ion)
-        ionify   .link     (ion)
+      ; ionify.id   (ion)
+      ; ionify.link (ion)
+
+      ; debug.push(["onION:",ion.re.id])
 
         var from = onObject.caller;
         ion.re.from || (ion.re.from = from && from.this && from.this.re.id);
@@ -328,43 +332,55 @@
               ;  if (!(ion [term] || term in ion)) continue next
               }
 
-            !  ionified [typeof   sense  [grammar]]
-            && ionified [typeof   ionify [grammar]]
-            && (sense [grammar] = ionify [grammar])
-            ;
+            !  ionified    [typeof sense  [grammar]] &&
+            (  ionified    [typeof ionify [grammar]] ||
+              "function" == typeof ionify [grammar]
+            )                                        &&
+            (  sense [grammar]  =  ionify [grammar])
 
             debug.push (["using",grammar,typeof sense [grammar]]);
 
             typeof  sense [grammar] == "function"
               ?  (  sense [grammar].ion != ion)
                  && sense [grammar]       (ion)
-              :  +  sense [grammar]
-              ;
+              :  ~  sense [grammar]
           }
 
         !ion.debug && ~{debug:debug}
 
         return this
-      },
+      }
 
 
-  EXPERIMENTS:
-    { methodBlock:
-        { re:
-            [/ note: methodBlock for creating a method with its own context /
-            ,/ note: This could simplify creating methods like +web.get     /
-            ,/ todo: ion.methodBlock = ion.methodBlock.is                   /
-            ],
-
-          get:
-            {data: 7},
-
-          is:
-            function ionMethod ()
-              {  this.get.data
-              }
-        }
+    ,
+  onType$:
+    { core: {Function:"function", Object:"object"}
+    , name: (/function\s+(.*)\s*\(/)
+    , undo: {}
     }
-} //+ionify
 
+    ,
+  onType:
+    function onType (ion)
+      { var on      = onType.ion
+          , known   = on.ionified
+          , onType$ = on.onType$
+          , core    = onType$.core
+          , name    = onType$.name
+          , undo    = onType$.undo
+          , Type    = ion.on
+          , type    = Type.name    || String(Type).match(name)[1]
+          ; undo [type]            =  Type.prototype.valueOf
+          ; Type.prototype.valueOf =  ion [type]
+          ; known [type]           =  true
+          ; known [core [type]]    =  !!core [type]
+          ~ {debug: ["onType",type,JSON.stringify(known)]}
+          ; return true
+      }
+
+
+}
+
++
+/on@ionify activated!/
 ;
