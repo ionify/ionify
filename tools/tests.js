5225d9f0e40f169f81f22260403b40918a8db05d
Mike Lee <git@iskitz.com>
2018-11-23 03:37:55 -0800

    docs: tests: tidied main loaders ~re & more.

--- tools/tests.js
+++ tools/tests.js
@@ -1,24 +1,24 @@
 ;
 ~
 { re:
-    { id:  "tests.0.1@ionify"
-    , is:  "ionify's test suite"
-    , by: ["Mike.Lee@ionify", "team@ionify"]
-    , at:  "2017.11.28-08...2016.05.18-07"
-    , in:  "san-jose.california.usa.earth"
-        ,
-      stories:
-        [ "todo: truly test emoji: ['0 . 0'] + ['0 . -'] + ['- . -']"
+    { id:  'tests@ionify'
+    , by: ['mike.lee', 'team']
+    , in:  'san-jose.california.usa.earth'
+    , on:  -7.20160518
+    , to:  -8.20181123
+    , is:  -0.1
+    , it:  "tests ionify"
+    , we:
+        [" were testing ionify's emoji actions: ['0 . 0'] + ['0 . -'] & ['- . -'] "
         ]
     }
 }
-
 +
-{ get:
-    [    "on.test@ionify",          "do.test@ionify"
-    ,   "get.test@ionify",      "errors.test@ionify"
-    ,   "log.test@ionify",         "use.test@ionify"
-    , "emoji.test@ionify", "punctuation.test@ionify"
+{ get
+:   [    'on.test@ionify',          'do.test@ionify'
+    ,   'get.test@ionify',      'errors.test@ionify'
+    ,   'log.test@ionify',         'use.test@ionify'
+    , 'emoji.test@ionify', 'punctuation.test@ionify'
     ]
 }
 ;
