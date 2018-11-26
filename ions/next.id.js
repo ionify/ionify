acb11253a3e6d455cd62ff1089e10e23bf5a2e93
Mike Lee <git@iskitz.com>
2018-11-26 05:26:19 -0800

    docs: ~next.id: shared with @ionify & tidied ~re & code

--- ions/next.id.js
+++ ions/next.id.js
@@ -1,31 +1,32 @@
 ;
 ~
 { re:
-    { id: "next.id@ionify"
-    , is: "An ion that generates sequential ids on-demand"
-    , by:["mike.lee@iskitz", "team@ionify"]
-    , on: -7.20160910
-    , to: -7.20170715
-    , at: +0.1
+    { id:  'next.id@ionify'
+    , by: ['mike.lee', 'team']
+    , at:  'ionify.net'
+    , on:  -7.20160910
+    , to:  -8.20181126
+    , is:  -0.1
+    , it: "generates sequential ids on-demand"
     }
 
+, on    : [['next'   ,      'id']]
+, share : { 'nextID' : "next id"}
+, MAX   : Number.MAX_SAFE_INTEGER ||  9007199254740991
+, MIN   : Number.MIN_SAFE_INTEGER || -9007199254740991
 , debug : false
-, on    : [["next", "id"]]
+, ids   : {}
 
-, MAX: Number.MAX_SAFE_INTEGER ||  9007199254740991
-, MIN: Number.MIN_SAFE_INTEGER || -9007199254740991
-, ids: {}
-
-, "next id":
-    function onNextId (ion)
-      { var th1s = onNextId.ion || this
-          , ids  = th1s.ids
+, "next id"
+:   function onNextId (ion)
+      { var thi$ = onNextId.ion || (onNextId.ion = onNextId == this["next id"] ? this : null)
+          , ids  = thi$.ids
           , name = ion.next
           , f0r  = ion.id.re || (ion.id.re = {})
           , id   = ids [name]
 
-      ; (id >= th1s.MAX)  && (id = ids [name] = null)
-      ; isNaN (id)        && (id = ids [name] = th1s.MIN)
+      ; (id >= thi$.MAX)  && (id = ids [name] = null)
+      ; isNaN (id)        && (id = ids [name] = thi$.MIN)
       ; f0r.id = name + '.' + id
       ; return ids [name]++
       }
