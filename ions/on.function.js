961e2c058c20f189f17edf0f82669a344cf70f8c
Mike Lee <git@iskitz.com>
2018-11-26 04:12:41 -0800

    docs: on.function: added ~re.we notes & tidied ~re & code

--- ions/on.function.js
+++ ions/on.function.js
@@ -1,30 +1,35 @@
 ;
 ~
 { re:
-    { id: "on.function.0.1@ionify"
-    , is: "An ion that handles activated functions; i.e. +go()"
-    , by:["mike.lee@iskitz", "team@ionify"]
-    , at: "2017.07.15-07...2007.09-04"
-        ,
-      stories:
-        [ /note: .../
-        , /todo: .../
-        , /bugs: ~[777].pop = Array.prototype.pop() = TypeError; this != [777]/
-        + /       Maybe sense objects without own method then create own method/
-        + /       with .ion=object that calls its inherited method with this=ion/
-        , /idea: ~function with this.apply() can do interesting things like/
-        + /       calling itself on itself! Not sure when that'd be  valuable/
-        + /       but interesting to try!/
+    { id:  'on.function@ionify'
+    , by: ['mike.lee', 'team']
+    , at:  'ionify.net'
+    , on:  -4.200709
+    , to:  -8.20181126
+    , is:  -0.1
+    , it:
+        [/ ionifies functions; e.g. enabling ~go which does go()                       /
+        ,/ provides access to the global object via ~function(){ return this }         /
+        ]
+    , we:
+        [/ will create or identify any existing function id; read ECMA-262 [] spec.    /
+        ,/ need to fix ~[777].pop = ~[777].prototype.pop() = TypeError; this != [777]. /
+        ,/ like to sense objects without their own method then create own method with  /
+        +/    .ion=object that calls its inherited method with this=ion.               /
+        ,/ like that ~Function + this.apply() can do interesting things like           /
+        +/    recursively applying itself! Not sure when that'd be valuable but        /
+        +/    interesting to explore 👨🏾‍💻🤓:                                            /
+        +             Object.prototype.toString.call ([777].__proto__)
         ]
     }
-    ,
-  on: Function,
-      Function:
-        function onFunction (ion)
+
+, on: Function
+,     Function
+:       function onFunction (ion)
           {  ion || (ion = this)
-             var result = ion() //ion.call (ion.ion)
-          ~  {debug: ['+', ion.name || "anonymous", '() : ', result]}
-             return result
+          ;  var result = ion() //ion.call (ion.ion)
+          ~  {debug: ['~', ion.name || 'anonymous', '() : ', result]}
+          ;  return result
           }
 }
 ;
