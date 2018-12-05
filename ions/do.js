50cb4e76b1114b1ad05460ed1e61176c4b3d9ee9
Mike Lee <git@iskitz.com>
2018-12-05 01:13:23 -0800

    code & docs: --do.-hard-coded --> shared ionified, reordered methods & tidied ~re's & code

--- ions/do.js
+++ ions/do.js
@@ -1,59 +1,35 @@
 ;
-
 ~
 { re:
-    { id: 'do@ionify'
-    , is: "ionify's do action: activates ions, functions + methods"
-    , by:['mike.lee@iskitz','team@ionify']
-    , in:['forest-hills.new-york.usa.earth'
-         ,  'san-jose.california.usa.earth'
-         ]
-    , on: -4.200709
-    , to: -7.20180423
-    , at: -0.1
+    { id:  'do@ionify'
+    , by: ['mike.lee','team']
+    , at:  'ionify.net'
+    , in: ['forest-hills.new-york.usa.earth'
+          ,  'san-jose.california.usa.earth'
+          ]
+    , on:  -4.200709
+    , to:  -7.20181205
+    , is:  -0.1
+    , it: "implements ~do, which activates ions, functions & methods"
     }
 
-    ,
-  on: 'do'
+, on: 'do'
 
-    ,
-  errors:
-    { ionExpected: "+do expects an ion or ion id"
-    }
-
-    ,
-  doStories:
-    [/ todo: Enable +{on: "" || [], do: ...}                      /
-    ,/ todo: ionified [typeof todo] > +{is:todo, not:"ionified"}  /
+, doInfo
+:   [/ will Enable +{on: '' || [], do: ...}                       /
+    ,/ like ionified [typeof todo] --> ~{is:todo, not:'ionified'} /
     ]
-    ,
-  do:
+, do:
     function onDo (ion)
-      { ion || ~onDo.ion.errors.ionExpected
-
-        var DO   = onDo.ion
-          , todo = ion.do
-
-      ; todo && !DO.ionified [typeof todo] && (todo = ion [todo])
+      { ion  || ~onDo.ion.errors.ionExpected
+      ; var todo    = ion.do
+      ; todo && !onDo.our.ionified [typeof todo] && (todo = ion [todo])
       ~ todo
       ; return true
       }
 
-    ,
-  ionified:
-    { Array    :  true
-    , boolean  : !true
-    , Error    :  true
-    , function :  true
-    , Function :  true
-    , number   : !true
-    , object   :  true
-    , Object   :  true
-    , RegExp   :  true
-    , string   : !true
-    , undefined: !true
+, errors
+:   { ionExpected: "~do expects an ion or ion id"
     }
-
-} //+do@ionify
-
+}
 ;
