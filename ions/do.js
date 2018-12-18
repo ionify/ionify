e4f093ac7a9075fd372e88c4172f954fb45a52cc
Mike Lee <git@iskitz.com>
2018-12-18 03:38:36 -0800

    code & docs: ~do: updated to resolve references to actions + tidied ~re

--- ions/do.js
+++ ions/do.js
@@ -8,7 +8,7 @@
           ,  'san-jose.california.usa.earth'
           ]
     , on:  -4.200709
-    , to:  -8.20181214
+    , to:  -8.20181218
     , is:  -0.1
     , it: "implements ~do, which activates aeons, functions, ions & pions"
     }
@@ -16,14 +16,14 @@
 , on: 'do'
 
 , doInfo
-:   [/ will Enable +{on: '' || [], do: ...}                         /
-    ,/ like ionified [typeof todo] --> ~ {is:todo, not:'ionified'}  /
+:   [" will Enable +{on: '' || [], do: ...}                        "
+    ," like ionified [typeof todo] --> ~ {is:todo, not:'ionified'} "
     ]
 , do:
     function doing (ion)
       { ion   || ~ doing.ion.errors.ionExpected
       ; var todo = ion.do
-      ; todo  && ! doing.our.ionified [typeof todo] && (todo = ion [todo])
+      ; todo  && ! doing.our.ionified [typeof todo] && ~{find:todo, in:ion, as:'do'} && (todo = ion.do)
       ~ todo
       ; return true
       }
