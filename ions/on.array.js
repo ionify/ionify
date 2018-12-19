876d78af37f633ed8590e5e321db027b911dec2c
Mike Lee <git@iskitz.com>
2018-12-18 22:59:01 -0800

    code & docs: ~[]: resolves references, senses aeon types, ...
    • .my: encapsulates ion's internals
    • intuitively named methods
    • ~on.as.do: ionify aesop emoji idea
    • tidied ~re

--- ions/on.array.js
+++ ions/on.array.js
@@ -1,85 +1,123 @@
 ;
-
 ~
 { re:
     { id:  'on.array@ionify'
     , by: ['mike.lee', 'team']
     , at:  'ionify.net'
     , on:  -4.200709
-    , to:  -8.20181201
+    , to:  -8.20181218
     , is:  -0.1
     , it:
         [/ Senses all array-expressed ions like:                   /
         +"   ['~ . ~']  &  [{0 : 0}]  &  [/d(~ . ~)b/]  &  [0 . 0] "
         ]
     , we:
-        [" were refactoring onArray & onAESOP into their own ion(s) now that we've   "
-        +" created domain spaces. They'll be part of the @ionify domain space which  "
-        +" shares the sense & ionified types maps.                                   "
-
+        [" were debugging ~find'ing aeon sensors like aesop "
         ,/ want shared emojii actions to resolve via: 1:on.array --> 2:@ionify-space /
         ]
     }
 
-, share
-:   { '0 . 0': 'activate'
-    , '0 . -': 'disable'
-    , '- . 0': 'disable'
-    , '- . -': 'deactivate'
+, my:
+    { sensors
+    :   { string    : 'aesop'
+      //, number    : ''
+      //, boolean   : ''
+      //, null      : ''
+      //, undefined : ''
+        }
+    , link
+    :   function link (ion)
+          { ion || (ion = link.ion)
+            for (var  member  in ion)
+              { member = ion [member]
+              ; if (member.my || ('my' in member)) continue
+              ; member.my = ion.my
+              }
+          }
     }
+
+, valueOf
+:  function hiphop ()
+     { this.my.link.ion = this.my.sensors.ion = this
+     ; this.my.link ()
+     ; delete this.valueOf && ~{link:'my', to:this} >> this
+     }
+
+, dont:
+    [ {on: '0 . 0', as:'aesop', do:'activate'  }
+    , {on: '0 . -', as:'aesop', do:'disable'   }
+    , {on: '- . 0', as:'aesop', do:'disable'   }
+    , {on: '- . -', as:'aesop', do:'deactivate'}
+    ]
 
 , on: Array
 ,     Array
-:       function onArray (ion)
-          { ion || (ion = this)
+:       function aeon (array)
+          { array ||  (array = this)
 
-          ~ {debug: ["~[",ion,"]"]}
+          ~ {debug: ["~[", array ,"]"]}
 
-            var aesop = onArray.ion.onAESOP
-              , our   = onArray.our
-              , next  = -1
-              , last  = ion.length
+            var AEON    = aeon.ion
+              , sensors = AEON.my.sensors
+            //, aesop   = aeon.ion.aesop
+              , ionified = aeon.our.ionified
+              , next    = -1
+              , last    = array.length
               , thing
+              , sense
               , type
-
-          //our.id   (ion)
-            our.link (ion)
+            //; our.ionified ['string'] = true
+              ;
 
             while (++next < last)
-              { thing = ion [next]
-                if (!thing)               continue
-                if (+thing && thing.did)  continue
-                ion.aesop = next
-                aesop (ion)
-              } delete ion.aesop
+              { thing =   array [next]
+              ; type  =  typeof  thing
+              ; sense = sensors [type]
+              ; if (!thing)      continue
+              ; if (ionified    [typeof thing]  && !('ion' in thing)) thing.ion = array
+              ; if (!sense)     {~thing; continue }
+              ; if (!ionified   [typeof sense])
+                  { ~{find:sense, in:sensors ||AEON, as:type}
+                  ;   sense = (sensors ||AEON) [type ||sense]
+                  ;   if (!sense)
+                        { ~{warn:`aeon: couldn't sense ${type} ~${JSON.stringify (thing)}`}
+                        ;   continue
+                        }
+                  }
+            //; if (!our.ionified [type])  continue
+            //;/*if (!thing.ion && !('ion' in thing))*/ (thing.ion = array)
+            //; if (+thing  && thing.did)  continue
+              ; sense.next = next // array.aesop = next
+              ; sense (array)
+              }// delete array.aesop
 
-            return next / ion.length
+            return next / array.length
           }
 
 
-, onAESOPInfo
+, aesopInfo
 :   [ /note: aesop: array-embedded statement|sentence|storie or phrase|pattern/
     , /todo: find known words in each sentence/
     , /todo: interpret via sentence(s), paragraph(s), chapter(s) + book(s)/
     , /todo: handle ['0 . 0'] via sense [thing] && sense [thing] (thing)/
     , /todo: use tbd name-to-ion resolver/
     ]
-, onAESOP
-:   function onAESOP (ion)
-      { ion || (onAESOP == this.onAESOP) && (ion = this)
+, aesop
+:   function aesop (ion)
+      { ion || (aesop == this.aesop) && (ion = this)
 
-        var phrase = ion [ion.aesop]
-        phrase && ~{debug: ["+[",phrase,"]"]}
+      ; var phrase = ion [aesop.next]
+      ; aesop.next = void 0
+      ; phrase && ~{debug: ["+[", phrase ,"]"]}
 
-        var sense     = ion.ion && ion.ion [phrase]
-          , shared    = onAESOP.our
-          , aesop     = onAESOP.ion
-          , ionified  = shared.ionified
-
-        !sense                      && (sense = shared .sense [phrase])
-        !ionified [typeof sense]    && (sense = shared [sense])
-        typeof sense == "function"  ?   sense  (ion) : ~sense
+      ; var sense     = ion.ion && ion.ion [phrase]
+        //, shared    = aesop.our
+        //, aesop     = aesop.ion
+        //, ionified  = shared.ionified
+          ;
+      ! sense                      && ~{find:phrase, in:ion} && (sense = ion[phrase])//(sense = shared .sense [phrase])
+    //! ionified [typeof sense]    && (sense = shared [sense])
+      ; typeof sense == "function"  ?   sense  (ion) : ~sense
       }
 }
-
 ;
