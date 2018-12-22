7cc4aff2b6e9b066266a6fe1cd44e5c70d77b276
Mike Lee <git@iskitz.com>
2018-12-22 06:21:57 -0800

    code & docs: ~on.no: now actually disables sensor

--- ions/on.no.js
+++ ions/on.no.js
@@ -5,11 +5,11 @@
     , by: ['mike.lee', 'team']
     , at:  'ionify.net'
     , on:  -4.200709
-    , to:  -8.20181218
+    , to:  -8.20181222
     , is:  -0.1
     , it:
-        [" implements ~on.no which enables disabling a sensor mapped to one or "
-        +" more term(s).                                                       "
+        [" implements ~on.no which disables a sensor mapped to one or more "
+        +" term(s).                                                        "
         ]
     , we:
         [" like the idea of resolving ~ {no: 'this.ion'}     "
@@ -22,27 +22,23 @@
 ,"on no"
 :"no"
 , no:
-    function no (ion)
-      { if (!ion.no && !('no' in ion)) return ion;
+    function no (sensor)
+      { if (!sensor.no && !('no' in sensor)) return sensor
 
-        var acts = ion.no
+        var  acts = sensor.no
           ; !Array.isArray (acts) && (acts = [acts])
           ;
-        var id
-          , act
-          , nion //next ion
+        var act
           , term
-          , known = no.our.known
           , next  = -1
           , last  = acts.length
+          , known = no.our.known
           , sense = no.our.sense
           ;
         while (++next < last)
-          { nion  =   acts [next].ion
-          ; term  =   ion.on
-          ; id    =   nion && nion.re && nion.re.id
+          { term  =   sensor.on
           ; act   =   sense [term]
-          ; act   &&  act.ion && (id && id == act.ion.re.id)
+          ; act   && (act == acts  [next])
                   &&  delete sense [term]
                   &&  delete known [term]
           }
