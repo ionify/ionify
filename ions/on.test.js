aa6949a11b6a8fa4085113f9ba3b2dc3239e8d82
Mike Lee <git@iskitz.com>
2019-01-01 02:16:40 -0800

    code: tests: ~on: tidied syntax & " --> ' for non-freeform text

--- ions/on.test.js
+++ ions/on.test.js
@@ -5,18 +5,19 @@
     , by: ['mike.lee', 'team']
     , at:  'ionify.net'
     , on:  -4.200709
-    , to:  -8.20181222
+    , to:  -8.20190101
     , is:  -0.1
     , it: "tests ionify's ~on api"
     }
 }
 
 ~
-{ on: "hi"
-, hi:
-    function onHi (ion)
-      { ~{log:"hi " + String (ion.hi) + "!"}
-      }
+{ on
+:   'hi'
+,    hi
+: function onHi (ion)
+    { ~{log:"hi " + String (ion.hi) + "!"}
+    }
 }
 
 ~
@@ -25,34 +26,40 @@
 , {hi:      "['0 . 0']"       }
 ]
 
+
 ~/... Test on:id ...................../
 +
-{ on:"ion@doma.in"
-, "ion@doma.in":
-    function onIonId (ion)
-      { ~{log: ["on:id:", ion.re.is, "with", ion.data, "works!"]}
-      }
+{ on
+:   'ion@doma.in'
+,   'ion@doma.in'
+: function onIonId (ion)
+    { ~{log: ["on:id:", ion.re.it, "with", ion.data, "that works!"]}
+    }
 }
+
 
 ~/... with a standard re.id ion ....../
 +
 {re
-: { id: "ion@doma.in"
-  , is: "an re.id ion"
-  , by: "mike.lee@iskitz"
-  , at: "2017.11.12-08...2017.11.11-08"
-  , in: "san-jose.california.usa.earth"
+: { id: 'ion@doma.in'
+  , by: 'mike.lee@ionify'
+  , on: -8.20171111
+  , to: -8.20190101
+  , in: 'san-jose.california.usa.earth'
+  , it: "is an re.id ion"
   }
 , data:"some data"
 }
 
+
 ~/... and a top-level-id ion ........./
 +
-{"ion@doma.in"
-:   { is: "a top-level-id ion"
-    , by: "mike.lee@iskitz"
-    , at: "2017.11.12-08...2017.11.11-08"
-    , in: "san-jose.california.usa.earth"
+{'ion@doma.in'
+:   { by: 'mike.lee@ionify'
+    , on: -8.20171111
+    , to: -8.20190101
+    , in: 'san-jose.california.usa.earth'
+    , it: "is a top-level-id ion"
     }
 , data:"some other data"
 }
@@ -61,9 +68,9 @@
 ~/... Test ~on:id,do ................./
 +
 { on:
-    [          "kana@ions.iskitz.net"
-    , ["wakatta.view@ions.iskitz.net", "show"]
-    , ["wakatta.game@ions.iskitz.net", "start", "stop", "guess", "next"]
+    [          'kana@ions.iskitz.net'
+    , ['wakatta.view@ions.iskitz.net', 'show']
+    , ['wakatta.game@ions.iskitz.net', 'start', 'stop', 'guess', 'next']
     ]
 , dont:
     [ ""
