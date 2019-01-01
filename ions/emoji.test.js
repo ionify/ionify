ccb1cd0d1d6275ec79d4e658daf9e7dc5d97dbb2
Mike Lee <git@iskitz.com>
2019-01-01 02:02:50 -0800

    docs: tests: emoji: uses live comment syntax

--- ions/emoji.test.js
+++ ions/emoji.test.js
@@ -5,14 +5,14 @@
     , by: ['mike.lee', 'team']
     , at:  'ionify.net'
     , on:  -4.200709
-    , to:  -8.20181123
+    , to:  -8.20181230
     , is:  -0.1
     , it:  "is ionify's emoji actions test suite"
     }
 }
 
-~['- . 0'] <= {warn:"~['- . 0']  <  /ionify isn't disabled/"}
-+['0 . 0'] <= { log:"~['0 . 0']  <  /ionify is     enabled/"}
--['0 . -'] <= {warn:"~['0 . -']  <  /ionify isn't disabled/"}
+~['- . 0'] <= {warn:"~['- . 0']  <=  /ionify isn't disabled/"}
++['0 . 0'] <= { log:"~['0 . 0']  <=  /ionify is     enabled/"}
+-['0 . -'] <= {warn:"~['0 . -']  <=  /ionify isn't disabled/"}
 &['0 . 0']
 ;
