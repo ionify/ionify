42eb0990276c74159d50ff29b2332e8c360a3ba2
Mike Lee <iskitz@users.noreply.github.com>
2019-01-04 16:24:08 -0800

    code: ions@ionify: moved to ./ions to enable ~get:'ions@ionify'

---
+++ ions/ions.js
@@ -0,0 +1,64 @@
+;
+~
+{ re:
+    { id: 'ions@ionify'
+    , by: 'mike.lee'
+    , at: 'ionify.net'
+    , in:
+        [ 'forest-hills.new-york.usa.earth'
+        ,   'san-jose.california.usa.earth'
+        ]
+    , on: -4.200709
+    , to: -8.20190104
+    , is: -0.1
+    , it:
+        [" configures ionify "
+        ," defines ion domains, paths & more "
+        ," provides domain lists as backup & performance options "
+        ]
+    , we:
+        [" like ions@ionify in main directory with hosts like web@ionify "
+        ]
+    },
+
+  do:
+    [ { with : '@ionify'
+      , share: '*'
+      , re   : false
+      , do   : false
+      }
+    ],
+
+  domains
+  : { verify : true
+    , resolve:
+        { ''             : '@ionify'
+        , '@'            : '@ionify'
+        , '@(.+)\.ionify': '@ionify/$1'
+        , '@ionify':
+              [ 'cdn.jsdelivr.net/gh/ionify/ionify@public/'
+              , 'ionify.github.io/ionify/'
+              , 'ionify.glitch.me/'
+              , 'ionify.net/'
+              , 'ionify.org/'
+              ]
+        }
+    },
+
+  paths
+  : {  ions: './ions/'
+    ,  team: './team/'
+    , notes: './notes/'
+    , tests: './tests/'
+    , tools: './tools/'
+    },
+
+  logging
+  : { debug : false
+    , error : true
+    , info  : false
+    , log   : false
+    , warn  : true
+    }
+}
+;
