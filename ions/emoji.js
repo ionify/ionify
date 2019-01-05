bc7b6b308e755b284ef4f6946f60b7921ada6817
Mike Lee <iskitz@users.noreply.github.com>
2019-01-04 17:30:30 -0800

    code: emoji@ created to implement ionify's emoji actions

---
+++ ions/emoji.js
@@ -0,0 +1,26 @@
+;
+~
+{ re:
+    { id:  'emoji@ionify'
+    , by: ['mike.lee', 'team']
+    , at:  'ionify.net'
+    , on:  -4.200709
+    , to:  -8.20190104
+    , is:  -0.1
+    , it:
+        [" senses specific aesop emoji that activate, disable & deactivate ionify i.e. "
+        +"   ['0 . 0']  &  ['0 . -']  &  ['- . 0']  &  ['- . -'] "
+        ],
+      we:
+        [" want aemoji actions to resolve via: 1:on.array --> 2:@ionify-space "
+        ]
+    },
+
+  do:
+    [ {on: '0 . 0', as:'aesop', do:'activate'  }
+    , {on: '0 . -', as:'aesop', do:'disable'   }
+    , {on: '- . 0', as:'aesop', do:'disable'   }
+    , {on: '- . -', as:'aesop', do:'deactivate'}
+    ]
+}
+;
