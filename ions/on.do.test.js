1799018aa7b76e129dce42fd23bbf239cf13e5c7
iskitz <git@iskitz.com>
2020-10-31 17:06:28 -0700

    test & patch: ~on.as.do: +tests & on.ion@id+link:true workaround

--- ions/on.do.test.js
+++ ions/on.do.test.js
@@ -5,44 +5,68 @@
   , is:  'examination'
   , by: ['mike.lee', 'team']
   , on:  -4.200709
-  , to:  -7.20201030
+  , to:  -7.20201031
   , at:  -0.1
   , it:
-      [ " tests ionify's ~on.as.has.id.is.do.no actions "
+      [ "tests ionify's ~on.as.has.id.is.do.no actions"
       ],
     we:
-      [ " were just fnishing ~on.has.do "
-      , " must ..."
-      , " will implement ~on.as.do or ~on.id.do next"
-      , " like ..."
-      , " have ..."
-      , " wont ..."
+      [ "were examining ~on.as ¿partial 🐛 matching?"
+      , "must ..."
+      , "like ..."
+      , "have ..."
+      , "wont ..."
       ]
   },
 
   do:
-    [ "Test ~on.as.has.id.is.do.no actions"
+    [ "Make ~on.as.has.id.is.do.no sensors"
+    , "Test ~on.has.do"
+    , "Test ~on. as.do"
     ],
 
-  "Test ~on.as.has.id.is.do.no actions":
-    function ondo_test ()
-      { +   {on:'ion',  as:[], do:Function}
-        |   {on:'ion',  as:[], no:Function}
-        |   {on:'ion', has:[], no:Function}
-        |   {on:'ion',  id:[], do:Function}
-        |   {on:'ion',  id:[], no:Function}
-        |   {on:'ion',  is:[], do:Function}
-        |   {on:'ion',  is:[], no:Function}
+  "Make ~on.as.has.id.is.do.no sensors":
+    function on_do_test_setup ()
+      { +   {on:'ion',  as:[], do:Object, id:!1, link:!1}
+        |   {on:'ion',  as:[], no:Object, id:!1, link:!1}
+        |   {on:'ion', has:[], no:Object, id:!1, link:!1}
+        |   {on:'ion',  id:[], do:Object, id:!1, link:!1}
+        |   {on:'ion',  id:[], no:Object, id:!1, link:!1}
+        |   {on:'ion',  is:[], do:Object, id:!1, link:!1}
+        |   {on:'ion',  is:[], no:Object, id:!1, link:!1}
+      },
 
-        +   {  on: 'ion'
-            , has:['this',['is','an'],['on','has'],'test']
-            ,  do:  function (ion) { console.log (`on.has: 👏🏾👨🏾‍💻🎉 ${Object.keys(ion)}`) }
-            }
+  "Test ~on.has.do":
+    function on_has_do_test ()
+      { function has (ion) { console.log (`on.has: 👏🏾👨🏾‍💻🎉 ${Object.keys (ion)}`) }
+        ~
+        {  on: 'ion'
+        , has:['this',['is','an'],['on','has'],'test']
+        ,  do:  has
+        , id:!1, link:!1
+        }
 
-        +   {this:0}
-        |   {is:0,an:false}
-        |   {on:0,has:0,test:0}
-        +   {test:0}
-  }
+        ~   {this:0           , id:!1, link:!1}
+        |   {is:0,an:false    , id:!1, link:!1}
+        |   {on:0,has:0,test:0, id:!1, link:!1}
+        +   {test:0           , id:!1, link:!1}
+
+        ~   {no:has, on:'ion' , id:!1, link:!1}
+      },
+
+  "Test ~on. as.do":
+    function on_as_do_test ()
+      { ~
+        { on: 'ion'
+        , as:['this','is','an','on','as','test']
+        , do:  function (ion) { console.log (`on.as : 👏🏾👨🏾‍💻🎉 ${ion.as}`) }
+        , id:!1, link:!1
+        }
+
+        ^ {as:['this','is','an','on','as','test']                 , id:!1, link:!1}
+        ~ {as:['this'], re:{as:'the real one'}                    , id:!1, link:!1}
+        | {as:['this','is','almost','as','test']                  , id:!1, link:!1}
+        + {as:['galore','this','is','an','on','as','test','more'] , id:!1, link:!1}
+      }
 }
 ;
