b1ad2a2cb786675aea80d113f3f496e0b76697c3
Mike Lee <iskitz@users.noreply.github.com>
2021-11-26 13:13:42 -0800

    code: onions: keeps domains & paths by the ions@'s domain
    includes
    : sets log-levels after keeping domains & paths in case used by logging
    : .re.* updated to spec
    : clear-statement-delimiters-style

--- ions/on.ions.js
+++ ions/on.ions.js
@@ -2,48 +2,51 @@
 ~
 { re:
     { id:  'on.ions@ionify'
-    , is:  'sensor'
+    , as:  'sensor'
     , by: ['mike.lee', 'team']
     , on:  -4.200709
-    , to:  -7.20201029
+    , to:  -8.20211126
     , at:  -0.1
-    , it:
-        [" senses ions@domain ions & applies & shares their settings.  "
-        ," ... "
+    , is:
+        [ "sensing ions@domain ions & applying & sharing their settings."
+        , "..."
         ],
       we:
-        [" were doing its initial implementation. "
-        ," will set ~on to sense id@'s.           "
-        ," will only override domains matching ions@'s domain.         "
-        ," will only override domains when ions@'s domain is verified. "
-        ," ... "
+        [ "WERE doing its initial implementation."
+        , "must fix ~on:ions@ overriding domains & paths because of common '' domain"
+        , "know domain-mapping depends on ~on.as.do for re.as.configuration."
+        , "must only override domains matching ions@'s domain.         "
+        , "must only override domains when ions@'s domain is verified. "
+        , "will set ~on to sense id@'s."
+        , "..."
         ]
     },
 
   on:
-   'ions@', // and-or {as:'setup'}
-   'ions@':
-    function onions (setup)
-      { var our     = onions.our
-          , domain$ = our.domains || (our.domains = {})
-          , path$   = our.paths   || (our.paths   = {})
-          , domains = setup.domains
-          , paths   = setup.paths
+   'ions@', // and-or {as:'configuration'||'setup'}
+   'ions@':function
+    onions (setup)
+      { var next
+          , our     = onions.our
+          , domain$ =   our.domains || (our.domains = {})
+          , path$   =   our.paths   || (our.paths   = {})
+          , domains = setup.domains || {}
+          , paths   = setup.paths   || {}
           , domain  = setup.re.id.match (/@(.*)$/) [1]
-          , next
-
-      ~ setup.logging
-
+          ; domain$ = domain$ [domain] ||
+                     (domain$ [domain]  = domains)
+          ; path$   = path$   [domain] ||
+                     (path$   [domain]  = paths)
+/*
         for (next in domains)
           { domain$ [next] = domains [next]
           }
 
-        path$  =  path$ [domain]
-              || (path$ [domain] = {})
-
         for (next in paths)
           { path$ [next] = paths [next]
           }
+*/
+      ~ setup.logging
       }
 }
 ;
