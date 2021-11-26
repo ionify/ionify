b6c31e8c4502f3ccb42cbb0c57a46ad787f7b00f
Mike Lee <iskitz@users.noreply.github.com>
2021-11-26 12:39:48 -0800

    tests: ~errors: revised to ensure its effective & unobtrusive testing
    includes
    : generated error catching & reporting
    : .re.* updated to spec
    : clear-statement-delimiters-style

--- ions/on.errors.test.js
+++ ions/on.errors.test.js
@@ -2,24 +2,36 @@
 ~
 { re:
     { id:  'on.errors.test@ionify'
+    , of:  'tests'
+    , as:  'test.0'
     , by: ['mike.lee','team']
-    , at:  'ionify.net'
     , on:  -7.20170414
-    , to:  -7.20190328
-    , is:  -0.1
-    , it:  "tests ionify's ~errors action"
-    }
-  
-, valueOf
-:   function testErrors ()
+    , to:  -8.20211126
+    , at:  -0.1
+    , is:  "testing ionify's ~errors action"
+    },
+
+  valueOf:
+    function testErrors ()
       { var errors=
-          { error1: "1st error"
-          , error2: "✅ ~errors & ~errors.error"
-          , error3: "3rd error"
+          { error1: "creates error type objects"
+          , error2: "causes an error"
           }
 
-      ~ { errors:errors }
-      &   errors.error2
+      ~ {errors: errors}
+
+      ; (typeof errors.error1 == 'object')
+          ? ~{log: "✅ ~errors:       "+ errors.error1.message}
+          : ~{log: "❌ ~errors:       "+ errors.error1}
+
+        try
+          { errors.error2
+          & {log: "❌ ~errors.error: "+ String (errors.error2)}
+          }
+        catch (err)
+          {
+          ~ {log: "✅ ~errors.error: "+ String (err.message)}
+          }
       }
   }
 ;
