67efaa8e8045dd1b52ae6c0b5135cb88b5d3487f
Mike Lee <iskitz@users.noreply.github.com>
2021-11-26 12:43:19 -0800

    code: ~errors: ensure no unintended error causing while error creating
    includes
    : avoids ion invoking during error creation
    : .re.* updated to spec
    : clear-statement-delimiters-style

--- ions/on.errors.js
+++ ions/on.errors.js
@@ -2,14 +2,15 @@
 ~
 { re:
     { id:  'on.errors@ionify'
-    , is:  'action'
+    , of:  'core'
+    , as:  'action'
     , by: ['mike.lee', 'team']
     , on:  -4.200709
-    , to:  -7.20201029
+    , to:  -8.20211126
     , at:  -0.1
-    , it:
-        [" converts messages to Error objects "
-        ," usage:                             "
+    , is:
+        [ "converting messages to Error objects"
+        , "usage:"
         ,     { errors:
                   { error1: "1st error"
                   , error2: "2nd error"
@@ -18,7 +19,7 @@
               }
         ],
       we:
-        [" will ... "
+        [ "will ..."
         ]
     },
 
@@ -27,7 +28,7 @@
         function onErrors (ion)
           { var next = ion.errors
             for (var error in next)
-              next [ error ] = new Error (next [error])
+              next [ error ] = new Error (String (next [error]))
           }
 }
 ;
