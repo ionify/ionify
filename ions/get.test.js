ec650c674fe02489da2d93861e5e9b220d23ad2c
Mike Lee <iskitz@users.noreply.github.com>
2021-11-26 09:51:48 -0800

    tests: +get.and's & tidied .re, remote protocols, logging & style
    includes
    : +get.and's for all action types
    : https:// <= http:// for all remote gets
    : ~get.remote logs ok-no-icons
    : +logged descriptions
    : updated .re to spec
    : clear-statement-delimiters-style++

--- ions/get.test.js
+++ ions/get.test.js
@@ -3,11 +3,10 @@
 { re:
     { id:  'get.test@ionify'
     , by: ['mike.lee', 'team']
-    , at:  'ionify.net'
     , on:  -4.200709
-    , to:  -7.20190413
-    , is:  -0.1
-    , it:  "Tests ionify's ~get api"
+    , to:  -8.20211125
+    , at:  -0.1
+    , is:  "testing ionify's ~get api"
     }
 }
 
@@ -15,69 +14,103 @@
 ~ {get:''}         <-  /testing getting the .js file/
 ~ {get:'@ionify'}  <-  /testing getting the @domain's .js file/
 
+~
+{ get:'../ions/i'
+, and:
+    { log: "✅ ~get script and    oli   {}"
+    }
+}
++
+{ get:'../ions/i'
+, and:
+    [{ log: "✅ ~get script and   aeon  [{}]"
+    }]
+}
++
+{ get:'../ions/i'
+, and:
+    [ {info:true}
+    , /✅ ~get script and   aeon  [\/\/,{}]/
+    ]
+}
++
+{ get:'../ions/i'
+, and:
+    /✅ ~get script and  storie  \/\//
+}
++
+{ get:'../ions/i'
+, and:function
+  and ()
+    { ~
+      /✅ ~get script and function ()/
+    }
+}
++
+{ info:false }
 
 ~
-{ get : 'http://ionify.net/ions/test/log.js'
+{ get : 'https://ionify.net/ions/test/log.js'
+, in  : 'order'
+, then:
+    { log: "✅ ~get script in order then oli {}"
+    }
+}
++
+{ get : 'https://ionify.net/ions/test/log.js'
 , in  : 'order'
 , then:
     function hi ()
-      {~{log: ["did ", hi.with.get]}
+      {~{log: ["✅ ~get script in order then function ()", hi.with.get]}
       }
 }
 
 
 ~
-/ testing grouped & ordered ~get's with a shared ~get.then /
-+
-{ re:" exploring syntax for ~get'ing non-ion scripts in order then using them "
-, get
-:   [ 'http://cdn.jsdelivr.net/gh/iskitz/ajile/use/com.iskitz.ajile.js?mvcoff,mvcshareoff'
-    , 'http://cdn.jsdelivr.net/gh/iskitz/ajile/play/api/scripts/com.iskitz.ajile.examples.LoadExample.js'
-    ]
-, in: 'order'
-, then
-:   function onLoadExample ()
+{ log:"testing ~get grouped & ordered with a shared ~get.then..."
+,  in: 'order'
+, get:
+    [ 'https://cdn.jsdelivr.net/gh/iskitz/ajile/use/com.iskitz.ajile.js?mvcoff,mvcshareoff'
+    , 'https://cdn.jsdelivr.net/gh/iskitz/ajile/play/api/scripts/com.iskitz.ajile.examples.LoadExample.js'
+    ],
+  then:
+    function onLoadExample ()
       {  ~com.iskitz.ajile.examples.LoadExample
       }
 }
 
 
 ~
-/ testing ~do-grouped individually ordered ~get's with 1 aliased & shared then /
-+
-{ re:
-    " exploring syntax for ~get'ing non-ion scripts in order then using them "
+{ log: "testing ~get's individually ~do-group ordered with 1 aliased & shared then"
 , do:
-    [ {  get: 'http://ajile.net/use/com.iskitz.ajile.js?mvcoff,mvcshareoff'
+    [ {  get: 'https://ajile.net/use/com.iskitz.ajile.js?mvcoff,mvcshareoff'
       ,   in: 'order'
-      }
-    , {  get: 'http://ajile.net/play/api/scripts/com.iskitz.ajile.examples.IncludeExample.js'
+      },
+      {  get: 'https://ajile.net/play/api/scripts/com.iskitz.ajile.examples.IncludeExample.js'
       ,   in: 'order'
       , then: 'doIt'
       }
-    ]
-, doIt
-:   function onIncludeExample ()
+    ],
+  doIt:
+    function onIncludeExample ()
       {  ~com.iskitz.ajile.examples.IncludeExample
       }
 }
 
 /*
 ~
-[" testing grouped & ordered individually mime-typed ~get's with a shared then. "
-," bug: emoji tests instantly disable ionify even before previous ~get.then's. "
-]
-+
-{ re:
-    " exploring syntax for ~get'ing non-ion scripts in order "
-, get
-:   [ {js:'http://ajile.net/use/com.iskitz.ajile.js?mvcoff,mvcshareoff'}
-    , {js:'http://ajile.net/play/api/scripts/com.iskitz.ajile.examples.LoadExample.js'}
+{ log:
+    [ "testing grouped & ordered individually mime-typed ~get's with a shared then. "
+    , "bug: emoji tests instantly disable ionify even before previous ~get.then's. "
+    ],
+  get:
+    [ {js:'https://ajile.net/use/com.iskitz.ajile.js?mvcoff,mvcshareoff'}
+    , {js:'https://ajile.net/play/api/scripts/com.iskitz.ajile.examples.LoadExample.js'}
     ]
 , as: 'text/javascript'
 , in: 'order'
-, then
-:   function onLoadExample ()
+, then:
+    function onLoadExample ()
       {  ~com.iskitz.ajile.examples.LoadExample
       }
 }
