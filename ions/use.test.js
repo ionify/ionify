9cd824020d64b80b28b1f041e42e5afe877129d3
Mike Lee <iskitz@users.noreply.github.com>
2021-11-26 12:47:01 -0800

    tests: ~use: revised for its effective & unobtrusive testing
    includes
    : test results with aligned icons & text
    : removed redundant & unneeded tests
    : .re.* updated to spec
    : clear-statement-delimiters-style

--- ions/use.test.js
+++ ions/use.test.js
@@ -3,40 +3,69 @@
 
 +
 { re:
-    { id: "use.test.0.1@ionify"
-    , is: "A suite of tests for ionify's +use actions"
-    , by: "mike.lee@ionify"
-    , at: "2017.07.11-07...2017.04.13-07"
+    { id:  'use.test.0.1@ionify'
+    , of:  'tests'
+    , as:  'test.0'
+    , by: ['mike.lee', 'team']
+    , on:  -7.20170413
+    , to:  -8.20211126
+    , at:  -0.1
+    , is: "testing ionify's ~use actions"
+    , we:
+        [ "were..."
+        , "must..."
+        , "will..."
+        , "need..."
+        , "want..."
+        , "hope..."
+        , "like..."
+        ]
     }
 }
 
 ~ /test +{use:"thing", as:"alias", in:someObject}/
-+ (say = {"🤓":"iamai", hellooo:7})
-+ {use:"hello", as:"hellooo", in:say}
++ (say = {"🤓":"iamai", hello:'👋🏾', hellooo:7})
++ {use : "hello", as:"hellooo", in:say}
 
-+ {hello:function
-   hello (name)
-     { name || (name = hello.to || "things")
-     ~ {log: "+['~ . ~'] < use " + String (name) + "!"}
-  }  }
++ { hello:function
+    hello (from)
+      { hello == say.hellooo
+          ? ~ {log: "✅ ~use named-thing          in object"}
+          : ~ {log: "❌ ~use named-thing          in object"}
 
-+  say.hello
+        hello == say.hellooo && say.hello == '👋🏾'
+          ? ~ {log: "✅ ~use named-thing as alias in object preserves name  in object"}
+          : ~ {log: "❌ ~use named-thing as alias in object preserves name  in object"}
+
+        hello == say.hellooo && typeof say.hellooo != 'number'
+          ? ~ {log: "✅ ~use named-thing as alias in object overrides alias in object"}
+          : ~ {log: "❌ ~use named-thing as alias in object overrides alias in object"}
+      }
+  }
 +  say.hellooo
-+ (say.hellooo.to = "Everything") + say.hellooo
 +  delete say
 
-~ /test +{use:"thing", in:someObject}/
-+ (game = {id:"wakatta.game@ionified"})
-+ {use:"show", as:"show", in:game}   <-   /todo: update +use to handle "use in"/
-~ {id:"wakatta.view@ionified", show:function show(){+{log:"わかった"}}}
-+ game.show
-+ delete game
+~ /test +use error conditions.../
 
-~
-[ /test +use error conditions/
-, {use:[], as:[], in:{}}
-, {use:"thing", as:"alias", in:"non-object"}
-]
+try
+  {
+  ~ {use: [], as:[], in:{}}
+  ~ {log: "❌ ~use error: can't use 'as' with more than 1 'use'"}
+  }
+catch (err)
+  {
+  ~ {log: "✅ ~use error: "+ String (err.message)}
+  }
+
+try
+  {
+  ~ {use: 'thing', as:'alias', in:"non-object"}
+  ~ {log: "❌ ~use error: can only use 'in' with objects"}
+  }
+catch (err)
+  {
+  ~ {log: "✅ ~use error: "+ String (err.message)}
+  }
 
 
 +
