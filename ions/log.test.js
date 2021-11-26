863e48db6edfec1284cbcff0e261a83c7c449dcd
Mike Lee <iskitz@users.noreply.github.com>
2021-11-26 09:56:27 -0800

    tests: {debug, error, info, log, warn}++ with all test results ensured
    includes
    : updated .re.* to spec
    : ~error test try-catch proposal

--- ions/log.test.js
+++ ions/log.test.js
@@ -2,27 +2,60 @@
 ~
 { re:
     { id:  'log.test@ionify'
-    , by: ['mike.lee','team']
-    , at:  'ionify.net'
+    , by: ['mike.lee@ionify.net','team']
     , on:  -4.200709
-    , to:  -8.20181222
-    , is:  -0.1
-    , it: "tests ionify's logging actions"
+    , to:  -8.20211126
+    , at:  -0.1
+    , is: "testing ionify's logging actions"
+    , we:
+        [ "must update with try-catch for ~{error} state resetting"
+        ]
+    },
+
+  valueOf :function
+  testing ()
+    { ~
+      [/ test +[], +debug, +error, +info, +log, +warn /
+      , (this.logstate = !!+{debug:''})
+      , {debug: false}
+      , {debug: "❌ ~debug test"}
+      , {debug: true}
+      , {debug: "✅ ~debug test"}
+      , {debug: this.logstate}
+      , (delete this.logstate)
+
+      , (this.logstate = !!+{info:''})
+      , {info: false}
+      , {info: "❌ ~info test"}
+      , {info: true}
+      , {info: "✅ ~info test"}
+      , {info: this.logstate}
+      , (delete this.logstate)
+
+      , (this.logstate = !!+{log:''})
+      , {log: false}
+      , {log: "❌ ~log test"}
+      , {log: true}
+      , {log: "✅ ~log test"}
+      , {log: this.logstate}
+      , (delete this.logstate)
+
+      , (this.logstate = !!+{warn:''})
+      , {warn: false}
+      , {warn: "❌ ~warn test"}
+      , {warn: true}
+      , {warn: "✅ ~warn test"}
+      , {warn: this.logstate}
+      , (delete this.logstate)
+
+    //, (this.logstate = !!+{error:''})
+      , {error: false}
+      , {error: "❌ ~error test"}
+      , {error: true}
+      , {error: "✅ ~error test"}
+      , {error: this.logstate}
+      , (delete this.logstate)
+      ]
     }
 }
-
-+
-[" test +[], +debug, +error, +info, +log, +warn "
-, { info: "~info  works"}
-
-, (this.ionifydebugteststate = !!+{debug:"get debug state"})
-, {debug: true}
-, {debug: "~debug works"}
-, {debug: ionifydebugteststate}
-, (delete this.ionifydebugteststate)
-
-, {  log: "~log   works"}
-, { warn: "~warn  works"}
-, {error: "~error works"}
-]
 ;
