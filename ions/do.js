56d7d3dd7b758d7e008037db0c56dc8ce8147702
Mike Lee <iskitz@users.noreply.github.com>
2021-12-03 22:20:20 -0800

    code & test: ~on.do: tidy & internal member renames to ++comprehension
    why
    : implementation is now easier to comprehend
    
    includes
    : underscored sensor names to more closely match action terms
    : clear-delimiters-style
    : re.* updated to spec
    
    tests
    : +on.do.after:all & :# tests
    : separated each test & its input for easier debugging

--- ions/do.js
+++ ions/do.js
@@ -11,14 +11,13 @@
     , is:
         [ "implementing ~on.do.after to do something after sensing specific ions"
         , "implementing: "
-        , "~ {   on:['ids'], do:action, after:'all'|'any'|'each'|'1..n'}"
-        , "+ {after:['ids'], do:action}  "
-        , "+ {on: String || [], do: ...} "
-        , "+ {do: String || []}          "
+        , "   ~ {   on:['ids'], do:action, after:'all'|'any'|'each'|'1..n'}"
+        , "   + {after:['ids'], do:action}  "
+        , "   + {on: String || [], do: ...} "
+        , "   + {do: String || []}          "
         ],
       we:
-        [ "were implementing & testing on.do.after.# sensor disabling..."
-        , "must ... "
+        [ "must ... "
         , "will ... "
         , "like ionified [typeof todo] --> ~ {is:todo, type:'ionified'} "
         , "like ... "
@@ -32,20 +31,22 @@
     ,           'do'
     ],
 
-  do:
-    function doing (ion)
-      { var todo  = ion.do
-      ;     todo && ! doing.our.ionified [typeof todo] && ~{find:todo, in:ion, as:'do'} && (todo = ion.do)
-      ;   ~ todo
-      ; return true
-      },
+  do    :function
+  doing (ion)
+    { var todo  =    ion.do
+    ;     todo  && ! doing.our.ionified [typeof todo]
+                && ~{find:todo, in:ion, as:'do'}
+                &&  (todo = ion.do)
+    ;   ~ todo
+    ; return true
+    },
 
- 'after do':
-    function afterDo ()
-      { this.on    = this.after
-      ; this.after = 'all'
-      ; afterDo.with.after.call (this)
-      },
+ 'after do':function
+  after_do ()
+    { this.on    = this.after
+    ; this.after = 'all'
+    ; after_do.with.after.call (this)
+    },
 
  'on do after':'after',
  'on do'      :'after',
@@ -60,103 +61,109 @@
     , undefined:'all'
     },
 
-  after:
-    function doAfter ()
-      { var ion   = this
-          , ions  = ion.on
-          , after = ion.after || 'all'
-          , own   = doAfter.with
-          , sense = own.ensure [after] || own.ensure [typeof after] || 'all'
-          , ready = own [sense] (ion)
-          , todo  = ion.do/*
-          ; todo  = doAfter.our.ionified [typeof todo]
-                  ?   todo
-                  : ~{find:todo, in:ion} && ion [todo]
-          ; todo || (todo = ion.do)*/
+  after :function
+  afteR ()
+    { var ion   = this
+        , ions  = ion.on
+        , after = ion.after || 'all'
+        , own   = afteR.with
+        , sense = own.ensure [after] || own.ensure [typeof after] || 'all'
+        , ready = own [sense] (ion)
+        , todo  = ion.do/*
+        ; todo  = doAfter.our.ionified [typeof todo]
+                ?   todo
+                : ~{find:todo, in:ion} && ion [todo]
+        ; todo || (todo = ion.do)*/
 
-        if (!todo) return void ~{warn:`~on.do.after: unable to find ${ion.do}`}
-        var on = {on:ions, after:after}
-        for (var next=-1, last=ions.length; ++next < last; on [ions [next]] = ready)
-        ion.do = ready
-      ~ on
-      },
+      if (!todo) return void ~{warn:`~on.do.after: unable to find ${ion.do}`}
+      var on = {on:ions, after:after}
+      for (var next=-1, last=ions.length; ++next < last; on [ions [next]] = ready)
+      ion.do = ready
+    ~ on
+    },
 
-  all:
-    function afterAll (ion)
-      { var go   = afterAll.with.go
-          , got  = {}
-          , ions = ion.on
-          , todo = ion.do
-          , them = {}
-          , done = false
-          ;
-        function afterAllIons ()
-          { if (done)  return
-            var   id = (this.re && this.re.id) || this.id
-            them[id] = this[id]
-            got [id] = true
-          ~ {debug: `\nget: ${ions}\ngot: ${Object.keys (got)}`}
-            for ( var next = -1
-                ,     last = ions.length
-                ;  ++ next < last
-                ;
-                ) if (!got [ions [next]]) return
-            done = true
-          ~ {on:ions, no:afterAllIons}
-            go ({do:todo, with:them})
-          }
-        return afterAllIons
-      },
+  all :function
+  all (ion)
+    { var go   = all.with.go
+        , got  = {}
+        , ions = ion.on
+        , todo = ion.do
+        , them = {}
+        , done = false
 
-  any:
-    function afterAny (action)
-      { var done = false
-          , todo = action.do
-          ,   go = afterAny.with.go
+      function
+      after_all ()
+        { if (done)  return
 
-        function afterAnyIon ()
-          { var ion = this
-          ~ {on:ion.re.id, no:afterAnyIon}
-            if (done) return
-            done = true
-            go ({do:todo, with:ion})
-          }
-        return afterAnyIon
-      },
+          var   id = (this.re && this.re.id) || this.id
+          them[id] = this[id]
+          got [id] = true
 
-  each:
-    function afterEach (ion)
-      { var todo = ion.do
-          , go = afterEach.with.go
+        ~ {debug: `\nget: ${ions}\ngot: ${Object.keys (got)}`}
 
-        function afterEachIon ()
-          { if (!this) return
-            ("function" == typeof todo && todo.length)
-            ?   todo (this)
-            : ~ todo <= / todo? remove duplicate todo from script.onload /
-          }
-        return afterEachIon
-      },
+          for ( var next = -1
+              ,     last = ions.length
+              ;  ++ next < last
+              ;
+              ) if (!got [ions [next]]) return
 
-  number:
-    function afterNumberOf (ion)
-      { var count = 0
-          , todo  = ion.do
-          , go = afterNumberOf.with.go
+          done = true
+            ~  {on:ions, no:after_all}
+          go ({do:todo, with:them})
+        }
 
-        function afterNumberOfIons ()
-          { if (isNaN (ion.after) || (ion.after >= ++count)) return
-            go ({do:todo, with:this})
-          }
-        return afterNumberOfIons
-      },
+      return after_all
+    },
 
-   go :function
-   go (todo)
-      { var can = ! ('in' in todo.do)
-        can && (todo.do.in = todo.with)
-             ~  todo.do
-        can && (todo.do.in = null)
-      }
+  go :function
+  go (todo)
+    { var can = ! ('in' in todo.do)
+      can && (todo.do.in = todo.with)
+            ~  todo.do
+      can && (todo.do.in = null)
+    },
+
+  any :function
+  any (action)
+    { var done = false
+        , todo = action.do
+        ,   go = any.with.go
+
+      function after_any ()
+        { var ion = this
+        ~ {on:ion.re.id, no:after_any}
+          if (done) return
+          done = true
+          go ({do:todo, with:ion})
+        }
+      return after_any
+    },
+
+  each :function
+  each (ion)
+    { var todo = ion.do
+        , go = each.with.go
+
+      function after_each ()
+        { if (!this) return
+          ("function" == typeof todo && todo.length)
+          ?   todo (this)
+          : ~ todo <= / todo? remove duplicate todo from script.onload /
+        }
+      return after_each
+    },
+
+  number :function
+  number (ion)
+    { var count = 0
+        , todo  = ion.do
+        , go = number.with.go
+
+      function after_number ()
+        { if (isNaN (ion.after) || (ion.after >= ++count)) return
+          go ({do:todo, with:this})
+        }
+      return after_number
+    }
 }
 ;
--- ions/do.test.js
+++ ions/do.test.js
@@ -2,31 +2,71 @@
 ~
 { re:
     { id:  'do.test@ionify'
+    , of:  'tests'
+    , as:  'test.0'
     , by: ['mike.lee', 'team']
-    , at:  'ionify.net'
     , on:  -4.200709
-    , to:  -8.20190109
+    , to:  -8.20211203
     , at:  -0.1
-    , it: "tests ionify's ~do actions"
+    , is: "testing ionify's ~do actions"
+    , we:
+        [ "must fix 🐛 ~on.do.after.all -8.20211203: only senses sets of unique@ids"
+        , "must fix 🐛 ~on.do.after.#   -8.20211203: only senses sets of unique@ids"
+        ]
     }
 }
 
-+
+~
+{ do:function()
+    { +{log:"✅ ~do:Function"}
+    }
+}
+
+~
 { do: 'hey'
-, hey:
-    function hi ()
-      { +{log:"✅ ~do:named:Function"}
-}     }
+, hey :function
+   hi ()
+      { +{log:"✅ ~do:Function:named"}
+      }
+}
+~ {do: {log:"✅ ~do:ion:{}"}}
+~ {do: 'went', went:{log:"✅ ~do:ion:{}:named"}}
+~ {do: [{log:"✅ ~do:[]"}] }
+~ {do: ['aesop'], aesop:{log:'✅ ~do:["aesop"]'}}
+~ {do: null}
 
-+
-{ do:
-    function ()
-      { +{log:"✅ ~do:Function"}
-}     }
 
-+ {do: {log:"✅ ~do:ion:{}"}}
-+ {do:'went', went:{log:"✅ ~do:named:ion:{}"}}
-+ {do: [ {log:"✅ ~do:[]"} ] }
-+ {do: ['aesop'], aesop:{log:'✅ ~do:["aesop"]'}}
-+ {do: null}
+~
+{ on: ['o@n.e','t@w.o','o@o.o'], after:'all'
+, do:function
+  go( actions)
+    { actions || (actions = go.in)
+    ; (   actions ['o@n.e'] == 1
+      &&  actions ['t@w.o'] == 2
+      &&  actions ['o@o.o'] == 0.0
+      )   ?  ~ {log:'✅ ~on.i@ds.do.after.all'}
+          :  ~ {log:'❌ ~on.i@ds.do.after.all'}
+    }
+}
+~
+[ {'o@n.e':1}
+, {'t@w.o':2}
+, {'o@o.o':0.0}
+]
+
+~
+{ on:['t@e.st']
+, after:2
+, do:function
+  go( action)
+    {  action || (action = go.in)
+    ; (action.test > 2) && ~{log:'✅ ~on.i@ds.do.after.#'}
+    ; (action.test < 2) && ~{log:'❌ ~on.i@ds.do.after.#'}
+    }
+}
+~
+[ {'t@e.st':1, test:1}
+, {'t@e.st':2, test:2}
+, {'t@e.st':3, test:3}
+]
 ;
