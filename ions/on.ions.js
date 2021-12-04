f5fe8e30215b800dddc8b203d5e0bbf2c3db5a69
Mike Lee <iskitz@users.noreply.github.com>
2021-12-03 23:58:35 -0800

    code: on.ions@ simplified implementation & tidied re.*

--- ions/on.ions.js
+++ ions/on.ions.js
@@ -2,51 +2,44 @@
 ~
 { re:
     { id:  'on.ions@ionify'
-    , as:  'sensor'
+    , of:  'core'
+    , as:  'sensation'
     , by: ['mike.lee', 'team']
     , on:  -4.200709
-    , to:  -8.20211126
+    , to:  -8.20211203
     , at:  -0.1
     , is:
-        [ "sensing ions@domain ions & applying & sharing their settings."
-        , "..."
+        [ "intended to sense ions@domain ions & apply & share their settings, but..."
+        , "currently senses single ions@ id which puts all configurations in an empty"
+        + "shared domain space."
         ],
       we:
-        [ "WERE doing its initial implementation."
+        [ "WERE doing its initial implementation"
         , "must fix ~on:ions@ overriding domains & paths because of common '' domain"
-        , "know domain-mapping depends on ~on.as.do for re.as.configuration."
-        , "must only override domains matching ions@'s domain.         "
-        , "must only override domains when ions@'s domain is verified. "
-        , "will set ~on to sense id@'s."
+        , "like ~on sensing ion.as to support ion.id:ions@doma.in for domain-configs"
         , "..."
         ]
     },
 
   on:
-   'ions@', // and-or {as:'configuration'||'setup'}
-   'ions@':function
-    onions (setup)
-      { var next
-          , our     = onions.our
-          , domain$ =   our.domains || (our.domains = {})
-          , path$   =   our.paths   || (our.paths   = {})
-          , domains = setup.domains || {}
-          , paths   = setup.paths   || {}
-          , domain  = setup.re.id.match (/@(.*)$/) [1]
-          ; domain$ = domain$ [domain] ||
-                     (domain$ [domain]  = domains)
-          ; path$   = path$   [domain] ||
-                     (path$   [domain]  = paths)
-/*
-        for (next in domains)
-          { domain$ [next] = domains [next]
+    [ 'ions@'
+    ],
+
+
+ 'ions@' :function
+  onions (configuration)
+      { var our    = onions.our
+          , space  = our.space (configuration.re.id)
+          , skip   = {re:true, on:true, do:true, get:true, 'ions@':true}
+
+        for
+          ( var setting in configuration)
+          {!skip [setting]
+              &&  configuration.hasOwnProperty (setting)
+              && (space [setting] = configuration [setting])
           }
 
-        for (next in paths)
-          { path$ [next] = paths [next]
-          }
-*/
-      ~ setup.logging
+        ~ configuration.logging
       }
 }
 ;
