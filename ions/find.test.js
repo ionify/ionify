67a34d7aa633baa6959a4ce58d49337cfd51c053
Mike Lee <iskitz@users.noreply.github.com>
2021-12-05 19:49:39 -0800

    tests: ~find: ++comprehension internal renames, re.* to spec & ' <= "

--- ions/find.test.js
+++ ions/find.test.js
@@ -1,46 +1,47 @@
 ;
 ~
-{'find.test@ionify'
-: { as:  'test'
+{'find.test@ionify':
+  { of:  'tests'
+  , as:  'test.0'
   , by: ['mike.lee', 'team']
-  , at:  'ionify.net'
   , on:  -8.20190107
-  , to:  -7.20190327
-  , is:  -0.1
-  , it:  "tests ionify's ~find.in.as action. "
-  , we: ["want tests to be done without being specified via a ~do."
-        ]
+  , to:  -8.20211205
+  , at:  -0.1
+  , is:  "testing ionify's ~find.in.as action. "
+  , we:
+      [ 'want tests to be done without being specified via a ~do.'
+      ]
   },
 
 do:
-  ["find name"
-  ,"find shared name"
-  ,"find alias"
-  ,"find shared alias"
-  ,"find aliases"
-  ,"find circular aliases"
+  [ 'find name'
+  , 'find name shared'
+  , 'find alias'
+  , 'find alias shared'
+  , 'find aliases'
+  , 'find aliases cyclical'
   ],
 
-"find name"
-: function name (tester)
-    { var state = name.our.logging.log
+'find name':function
+ find_name (tester)
+    { var state = find_name.our.logging.log
 
     ~ {do:['a']
       , a:[{log:true}
-          ,{log:"✅ ~find name"}
+          ,{log:'✅ ~find name'}
           ,{log:state}
           ]
       }
     },
 
-"find shared name"
-: function sharedName (tester)
-    { var state = sharedName.our.logging.log
+'find name shared':function
+ find_name_shared (tester)
+    { var state = find_name_shared.our.logging.log
       ~
       {'name@sharedfindtest':{}
       , share:{a:'a'}
       , a:[{log:true}
-          ,{log:"✅ ~find shared name"}
+          ,{log:'✅ ~find name shared'}
           ,{log:state}
           ]
       }
@@ -50,27 +51,27 @@ do:
       }
     },
 
-"find alias"
-: function aliased (tester)
-    { var state = aliased.our.logging.log
+'find alias':function
+ find_alias (tester)
+    { var state = find_alias.our.logging.log
 
     ~ {do: 'a'
       , a: 'b'
       , b:[{log:true}
-          ,{log:"✅ ~find alias"}
+          ,{log:'✅ ~find alias'}
           ,{log:state}
           ]
       }
     },
 
-"find shared alias"
-: function sharedAlias (tester)
-    { var state = sharedAlias.our.logging.log
+'find alias shared':function
+ find_alias_shared (tester)
+    { var state = find_alias_shared.our.logging.log
       ~
       {'alias@sharedfindtest':{}
       , share:{b:'b'}
       , b:[{log:true}
-          ,{log:"✅ ~find shared alias"}
+          ,{log:'✅ ~find alias shared'}
           ,{log:state}
           ]
       }
@@ -81,31 +82,31 @@ do:
       }
     },
 
-"find aliases"
-: function multi (tester)
-    { var state = multi.our.logging.log
+'find aliases':function
+ find_aliases (tester)
+    { var state = find_aliases.our.logging.log
 
     ~ {do: 'a'
       , a: 'b'
       , b: 'c'
       , c:[{log:true}
-          ,{log:"✅ ~find aliases"}
-          ,{log:multi.our.logging.log}
+          ,{log:'✅ ~find aliases'}
+          ,{log:find_aliases.our.logging.log}
           ]
       }
     },
 
-"find circular aliases"
-: function circular (tester)
-    { var state = circular.our.logging.log
+'find aliases cyclical':function
+ find_aliases_cyclical (tester)
+    { var state = find_aliases_cyclical.our.logging.log
 
     ~ {do: 'a'
       , a: 'b'
       , b: 'a'
       }
     + {log:true}
-    + {log:"✅ ~find circular aliases"}
-    + {log:circular.our.logging.log}
+    + {log:'✅ ~find aliases cyclical'}
+    + {log:find_aliases_cyclical.our.logging.log}
     }
 }
 ;
