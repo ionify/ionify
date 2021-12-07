8b831ea78371decd769c8257b04fd5213f78301d
Mike Lee <iskitz@users.noreply.github.com>
2021-12-07 06:47:48 -0800

    code: ids@ ~on:* handling & tidied
    includes
    : new generated id:  'log' id-prefix for log-level actions
    : internal  renames for ++comprehension
    : re.as: sensation: sensing action

--- ions/id.js
+++ ions/id.js
@@ -3,71 +3,77 @@
 { re:
     { id:  'id@ionify'
     , of:  'core'
-    , as:  'sensor'
+    , as:  'sensation'
     , by: ['mike.lee', 'team']
     , on:  -4.200709
-    , to:  -8.20211205
+    , to:  -8.20211206
     , at:  -0.1
     , is:
-        [ 'ensuring that all ionified objects have an re.id.                  '
-        , "setting an object's re.id value as a member mapped to its object re"
-        , 'setting missing re & re.id on an object with a domain-named member '
-        , 'setting missing re on a object.                                    '
-        , 'setting missing re.id via ~next.id when possible.	                '
-        ]
-        ,
+        [ 'ensuring that all ionified objects have an re.id.'
+        , "setting an object's re.id value as a member mapped to its object re."
+        , 'setting missing re & re.id on an object with a domain-named member.'
+        , 'setting missing re on a object.'
+        , 'setting missing re.id via ~next.id when possible.'
+        ],
       we:
-        [ "like updating ions with old ids once next.id's available"
-        , "will ensure that ~debug & ~next.id don't keep using setID.nextID "
+        [ 'were adapting to & testing ~on:* 🙏🏾'
+        , "have ID.nextID's range as opposite next.id's to minimize overlap."
+        , "like ions with old ids being updated once next.id's available."
         , "will 👨🏾‍💻 id.domain++ via id:{domain:'domain',toString:'id@domain'}"
         , "like linking this ion although it doesn't currently rely on it   "
         ]
     },
 
   on:
-    [ 'id'
-    ],
+    ['*'],
 
-  id:
-    function setID (ion)
-      { if (!ion.id || typeof ion.id != 'boolean') return
+ '*' :function
+  ID (ion)
+    { // 🐛 via on.ion@onion.id:true workaround
+      ;('boolean' == typeof ion.id) && delete ion.id
 
-        // 🐛 via on.ion@onion.id:true workaround
-        ;('boolean' == typeof ion.id) && delete ion.id
+      var id  =  ion.hasOwnProperty ('re')
+              ?  ion.re.id
+              : (ion.re = {id: void 0}).id
+              ;
 
-        var id =  ion.hasOwnProperty ('re')
-               ?  ion.re.id
-               : (ion.re = {id: void 0}).id
-               ;
+      if(id || isFinite (id))
+        return   ion [id]
+              ?       id
+              : (ion [id] = ion.re).id
+              ;
 
-        if (id || isFinite (id))
-          return  ion [id]
-               ?       id
-               : (ion [id] = ion.re).id
-               ;
+      for (var word in ion)
+        if (~ word.search (/@/))
+          { var    re  = ion [id = ion.re.id = word]
+          ; typeof re == 'object'
+              &&  (ion .re = re)
+              && !('id' in   re)
+              &&  ( re .id = id)
+          ; return id
+          }
 
-        for (var word in ion)
-          if (~ word.search (/@/))
-            { var    re  = ion [id = ion.re.id = word]
-            ; typeof re == 'object'
-                &&  (ion .re = re)
-                && !('id' in   re)
-                &&  ( re .id = id)
-            ; return id
-            }
+      var named
+        = (ion.debug ||  ion.log || ion.error || ion.info || ion.warn)
+        ? 'log' : 'ion'
 
-        !ion.re.id && !(ion.next && ion.id) && !ion.debug && ~{next:'ion', id:ion}
+      ion.re.id
+        ||  (ion.next && ion.id) /*|| ion.debug*/
+        || ~{next:named, id:ion}
 
-        !   ion.re.id
-      //&& !ion.debug || (ion.re.as != 'logger')
-      //&& (console.log ('no ~next.id; debug?', !!ion.debug, 'using '+setID.nextId),true)
-        && (ion.re.id = 'ion.'
-                      + (setID.nextId ? ++setID.nextId
-                                      :  (setID.nextId = 1)))
-                                      + '@'
-        id = ion.re.id
-        !ion [id] && (ion [id] = ion.re)
-        return id
-      }
+      ion.re.id
+      //||  ion.debug || (ion.re.as == 'logger')
+      //|| (console.log ('no ~next.id; debug?', !!ion.debug, 'using '+ID.nextId),true)
+        || (ion.re.id = named/*----*/+ '.' +/*----------------------------------*/
+         /*|*/          ( ID.nextId  ? --ID.nextId                              //
+         /*|*/                       :  (ID.nextId = Number.MAX_SAFE_INTEGER    //
+         /*|*/                                    || 9007199254740991           //
+           )/*--------*/)/*-----------*/)/*-------------------------------------*/
+
+      ion [id  = ion.re.id] ||
+     (ion [id] = ion.re   )
+
+      return id
+    }
 }
 ;
