b2a74444073eedd51dff521a18b1fdbf52fe5e68
Mike Lee <iskitz@users.noreply.github.com>
2021-12-08 07:18:55 -0800

    codish: find.link.share@ i was completely out of it but merged it...
    i have no true understanding of what, what i merged, does. I'm still
    way too sleep-deprived & exhausted to tell :sleepy::open_mouth:‍:dash:🤤:dizzy_face:‍:dizzy::sleeping:

--- ions/find.link.share.js
+++ ions/find.link.share.js
@@ -73,53 +73,6 @@
 
  'find in as':'find',
  'find in'   :'find',
-  find:
-    function find (ion)
-      { var name      = ion.find
-      ,     to        = ion.in
-      ,     as        = ('as' in ion) ? ion.as : name
-      ,     ionified  = find.our.ionified
-      ,     context   = find.with
-      ,     found
-      ,     last
-
-      ; while
-          ( last != to)    // bug? might infinitely loop on circular .with's | .our's
-          { last  = to
-          ; if ( found   =  context.findName ({find:name, in:to})       ) break
-          ; if ( to.with && ionified [typeof (found = to.with [name]) ] ) break
-          ; if ( to.our  && ionified [typeof (found = to.our  [name]) ] ) break
-          ; if ( to.with ){ to = to.with } else break
-          }
-
-  valueOf :function
-  ionify  ()
-    {   this.find_link_share ()
-    ;   delete this.valueOf
-    ; ~ this
-    },
-
-  linkInfo:
-    [~/were implementing .our.* --> .with.doma.in.*               /
-    ,~/MUST fix callee.caller.our 🐛 exposing ionify's domain space/
-    ,~/like linking {}s & ~link keeping their id-mapped with & our/
-    ,' want .with .doma .in.expanded.name.of.shared.thing:        '
-    +"      .with$.doma$.in.expanded auto-added $'s on conflicts  "
-    ,' want to move id@domain matching to its own ion then share it'
-    ,' like id@domain matching with /(.*)([-+]\d+.*^@)|(@.*)/     '
-    ,' will enable   + {link:ion, to:thing}                       '
-    ,' like enabling ~ {link: true|false } with true as default   '
-    ,' like enabling ~ {link: [ion, object, more])                '
-    ,' like enabling ~ {link:ion, as:{member:thing}}              '
-    ],
- 'link as':'link',
-  link:
-    function link (ion)
-      { ion || (ion =  link.with || (link.with = (link == this.link) &&  this))
-                    //(link.of   || (link.of   = (link == this.link) && (this.of.my = this, this.of))).my)
-
- 'find in as':'find',
- 'find in'   :'find',
   find :function
   find (action)
     { var name      = action.find
@@ -139,8 +92,8 @@
         ; if ( to.with )  {                    to = to.with.its      } else break
         }
 
-    ; found  && (action.in [as] = found)
-    ; return !! found
+      found  && (action.in [as] = found)
+      return !! found
     },
 
   find_alias :function
@@ -156,7 +109,7 @@
         ; thing = place [thing]
         ; if (ionified  [typeof thing]) return thing
         }
-    ; return false
+      return false
     },
 
   linkInfo:
@@ -196,81 +149,57 @@
         ; domain    = domain && domain [1]
         ; id        = id.replace (/(.+)(@|\.\d\.).*/, '$1')
 
-      for (property in ion)
+      for
+        ( var property in ion)
         { thing = ion [property]
-          if (!thing)                                                 continue
-          if ((typeof thing != 'function') && !Array.isArray (thing)) continue
+          if (!thing || skip [property] || !can [typeof thing])       continue
+      /**/if ((typeof thing != 'function') && !Array.isArray (thing)) continue
           if (!ion.hasOwnProperty (property))                         continue
         //thing.our      ||   space   && (thing.our  = /*|| ion ||*/ space)
           debugging      &&
-          ((id != 'ion') && ion.debug || debug.push ('linked '+ id +'.'+ property))
+          ((it != 'ion') && ion.debug || debug.push ('\n\tlinked '+ it +' ['+ property +']'))
         } debugging      && ion.debug || debug.length &&  our.debug ({debug:debug})
 
       return true
-        var property
-          , thing
-          , debug = []
-          , can   = {function:true, object:true}
-          , my    = link./*of.my/*/with
-          , skip  = my.of.our['*'].skip
-          , id    = (ion.re ? ion.re.id : null) || 'ion@'
-          , domain= id.match(/@(.*)/)
-          , it    = id.replace (/(.+)(@|[-\.]\d\.).*/, '$1')
-          , space = my.getSpace (id)
-        //, space = my.get_sion_space ({id:id})
-          ; domain = domain ? domain[1] : ''
-
-        for (property in ion)
-          { thing = ion [property]
-            if (!thing || skip [property] || !can [typeof thing])       continue
-        /**/if ((typeof thing != 'function') && !Array.isArray (thing)) continue
-            if (!ion.hasOwnProperty (property))                         continue
-          //my.sion ({ion:ion, domain:domain, our:space})
-            thing.with  ||              (thing.with  = ion)
-          //thing.with  ||              (thing.with$ = ion)
-            thing.our   ||  space    && (thing.our   = /*(thing.with$||thing.with)[domain] = /*|| ion ||*/ space)
-            ion.debug   || (it != 'ion') && debug.push ('\n\tlinked '+ it +' ['+ property +']')
-          }
-
-        ion.debug || debug.length && ~{debug:debug}
-        return true
-      },
+    },
   sion :function
   sion (from)
-    { sion.of || (sion.of = (sion == this.sion) && (this.of.my = this, this.of))
+    { sion.with || (sion.with = (sion === this.sion) && this.with)
 
-      var my      = sion.of.my//with
-        , skip    = my.of.our['*'].skip
-        , spaces  = my.spaces
-        , our     = from.our || my.get_sion_space (domain)
-        , all     = spaces.null
+      var wiTh    = sion.with
+        , own     = wiTh.own
+        , self    = own.self
+        , skip    = wiTh.all.skip
+        , spaces  = own.spaces || (own.spaces = own.self.spaces)
+        , domain  = from.domain
+        , our     = from.our || wiTh.sion_space (domain)
+        , all     = wiTh.all || spaces.null
         , ion     = from.ion
-        , of      = ion.of  || (ion.of = {my:ion, our:our, '*':all})
+        , within  = ion.with || (ion.with = {my:ion, our:our, all:all})
         , ofs     = ion.re.of
-        ; domain  = from.domain
         , from    = {id:null, parent:null}
 
-                        of.my       || (of.my       =  ion)
-                        of.our      || (of.our      =  our)
-                        of.our['*'] || (of.our['*'] =  all)
-        Array.isArray ( ofs )       || (ofs         = [ofs])
+                  within.own  || (within.own  =  ion)
+                  within.our  || (within.our  =  our)
+                  within.all  || (within.all  =  all)
+        Array.isArray ( ofs ) || (ofs         = [ofs])
 
         for // link re.of spaces + space_domain <= space@domain alias
-          ( var S = ofs.length, s//,   fs
+          ( var S = ofs.length, s,   fs
           ;     S--, (s  = ofs [S]), S+1
           ; ~s.indexOf('@') || (s +='@'+domain)
-          ,  of.our[s] = spaces[s]  || my.get_sion_space ((from.id=s, from.parent=my.get_sion_space((from.id=s,from.parent=null,from)), from))
-        //,  fs = s.replace('@','_')
-        //,  of.our[fs] = spaces[fs] = of.our[s]
+          ,  within.our[s] = spaces[s]  || self.sion_space ((from.id=s, from.parent=self.sion_space((from.id=s,from.parent=null,from)), from))
+          ,  fs = s.replace('@','_')
+          ,  within.our[fs] = spaces[fs] = within.our[s]
           );
 
-        if( of.our == our ) return
+        if( within.our === our ) return
 
         for // link & merge of.our spaces
-          ( var name in of.our)
-          { s = of.our [name]
-          ; if(    our [name] == s || skip [name]/*|| !s*/) continue
-      /**/; if(!of.our.hasOwnProperty (name))               continue
+          ( var name in within.our)
+          { s = within.our [name]
+          ; if(    our [name]=== s || skip [name]/*|| !s*/)     continue
+      /**/; if(!within.our.hasOwnProperty (name))               continue
             if(!(name in our))  { our [name] = {}}//;           continue}
             for
               ( var m in s)
@@ -278,62 +207,27 @@
               ; our [name][m] = s[m]
               ;   s       [m] = null
               }
-        //;    our [name] = s
-          ; of.our [name] = null
-          } of.our        = our
+          ;    our [name] = s
+          ; within.our [name] = null
+          } within.our        = our
     },
 
   unlinkInfo:
     [ 'todo: enable +{unlink: ion, from: thing}'
     ],
-  unlink:
-    function unlink (ion)
-        { ion || (unlink == this.unlink) && (ion = this);
+  unlink :function
+  unlink (ion)
+    { ion || (unlink == this.unlink) && (ion = this);
 
-        var property
-            , thing
-            , id = (ion.re ? ion.re.id : void 0) || 'ion'
-            ; id = id.replace (/(.+)(@|\.\d\.).*/, '$1')
-            ;
-        for (property in ion)
-            {  thing = ion [property]
-            ;  (typeof thing == 'function') && (thing [id] == ion) && (delete thing [id])
-            }
-        },
-
-  shareInfo:
-    [' ... '
-    ," will fix ~share:'*' to resolve shared things; now assumes ~do:[{share:'*'}] "
-    ,' will create +{share: {thing:..., other:...}, with:[ion.ids]} '
-    ],
- 'link to':'share',
-  share:
-    function share (ion)
-      { var thi$   = share.with || (share == this.share ? this : null)
-          , spaces = thi$.spaces
-          , object = {object:true,function:true}
-          , things = ion.link ==  '*' ? ion.with.with  || ion.with || ion : ion.to ? ion.link : ion.share
-          , to     = ion.to   || (ion.re && ion.re.id) || ''
-          ; ('*'  == to) && (to = Object.keys (thi$.spaces))
-          ; !Array.isArray  (to) && (to = [to])
-
-        for (var thing in things)
-          { if (( ('boolean' == typeof ion [thing]) && !ion [thing])
-            ||  ( ('re'      ==             thing ) &&  ion.re !== true)
-            ||     ~thing.indexOf ('@') &&  true   !==  ion [thing])
-            continue
-
-            for
-              ( var T = to.length, name=thing, id, space, from={id:null}
-              ;     T--
-              ;    id = (id = to[T]) && object [typeof id] ? id.re && id.re.id : id
-              , space = thi$.getSpace (id)//.get_sion_space ((from.id=id, from))
-              , space [name]
-                  = typeof (thing = things [name]) == 'string'
-                  ?    ion [thing]
-                  :         thing
-              );
-          }
+    var property
+        , thing
+        , id = (ion.re ? ion.re.id : void 0) || 'ion'
+        ; id = id.replace (/(.+)(@|\.\d\.).*/, '$1')
+        ;
+    for (property in ion)
+        {  thing = ion [property]
+        ;  (typeof thing == 'function') && (thing [id] == ion) && (delete thing [id])
+        }
     },
 
   shareInfo:
@@ -350,16 +244,25 @@
         , spaces = thi$.spaces
         , things = ion.link ===  '*' ?  ion.with.its/*.with || ion.with*/ || ion : ion.to ? ion.link : ion.share
         , to     = ion.to   ||  (ion.re && ion.re.id) || ''
-        , space  = thi$.space (to)
-        ;
+        ; ('*' === to) && (to = Object.keys (thi$.spaces))
+        ; Array.isArray   (to)  || (to = [to])
+
       for (var thing in things)
-        { if ((('boolean' == typeof ion [thing]) && !ion [thing])
-          ||  ((thing == 're') && !ion.re))
+        { if (( ('boolean' == typeof ion [thing]) && !ion [thing])
+          ||  ( ('re'      ==             thing ) &&  ion.re !== true)
+          ||     ~thing.indexOf ('@') &&  true   !==  ion [thing])
           continue
-          space [thing]
-            = typeof (thing = things [thing]) == 'string'
-            ? ion    [thing]
-            :         thing
+
+          for
+            ( var T = to.length, name=thing, id, space, from={id:null}
+            ;     T--
+            ;    id = (id = to[T]) && object [typeof id] ? id.re && id.re.id : id
+            , space = thi$/*.space (id)/*/.sion_space ((from.id=id, from))
+            , space [name]
+                = typeof (thing = things [name]) == 'string'
+                ?    ion [thing]
+                :         thing
+            );
         }
     },
 
@@ -368,7 +271,8 @@
     , 'todo: ...'
     ],
   spaces:
-    { null: {}
+    { with: {all : null}
+    , null: {}
     },
 
   spaceInfo:
