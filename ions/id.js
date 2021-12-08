aec0a2408c161588899b799b09966286532979c9
Mike Lee <iskitz@users.noreply.github.com>
2021-12-07 21:36:10 -0800

    code: id@ simplified ~on:* registering, --id+link:bug: +go.links & tidied

--- ions/id.js
+++ ions/id.js
@@ -6,7 +6,7 @@
     , as:  'sensation'
     , by: ['mike.lee', 'team']
     , on:  -4.200709
-    , to:  -8.20211206
+    , to:  -8.20211207
     , at:  -0.1
     , is:
         [ 'ensuring that all ionified objects have an re.id.'
@@ -15,24 +15,29 @@
         , 'setting missing re on a object.'
         , 'setting missing re.id via ~next.id when possible.'
         ],
+      go:
+        { plan: 'https://github.com/ionify/ionify/projects/1?fullscreen=true'
+        , help: 'https://github.com/ionify/ionify/issues'
+        , code: 'https://github.com/ionify/ionify/blob/public/ions/id.js'
+        , team: 'https://github.com/ionify/about/tree/public/team'
+        , deal: 'https://github.com/ionify/ionify/blob/public/LICENSE.txt'
+        , more: 'https://api.ionify.net/'
+        },
       we:
-        [ 'were adapting to & testing ~on:* 🙏🏾'
-        , "have ID.nextID's range as opposite next.id's to minimize overlap."
+        [ "have ID.nextID's range as opposite next.id's to minimize overlap."
         , "like ions with old ids being updated once next.id's available."
         , "will 👨🏾‍💻 id.domain++ via id:{domain:'domain',toString:'id@domain'}"
         , "like linking this ion although it doesn't currently rely on it   "
         ]
     },
 
-  on:
-    ['*'],
+  on:'*',
+     '*':'ID'
+        ,
 
- '*' :function
+  ID :function
   ID (ion)
-    { // 🐛 via on.ion@onion.id:true workaround
-      ;('boolean' == typeof ion.id) && delete ion.id
-
-      var id  =  ion.hasOwnProperty ('re')
+    { var id  =  ion.hasOwnProperty ('re')
               ?  ion.re.id
               : (ion.re = {id: void 0}).id
               ;
@@ -62,17 +67,13 @@
         || ~{next:named, id:ion}
 
       ion.re.id
-      //||  ion.debug || (ion.re.as == 'logger')
-      //|| (console.log ('no ~next.id; debug?', !!ion.debug, 'using '+ID.nextId),true)
         || (ion.re.id = named/*----*/+ '.' +/*----------------------------------*/
          /*|*/          ( ID.nextId  ? --ID.nextId                              //
          /*|*/                       :  (ID.nextId = Number.MAX_SAFE_INTEGER    //
          /*|*/                                    || 9007199254740991           //
            )/*--------*/)/*-----------*/)/*-------------------------------------*/
 
-      ion [id  = ion.re.id] ||
-     (ion [id] = ion.re   )
-
+      ion [id = ion.re.id]  ||  (ion [id] = ion.re)
       return id
     }
 }
