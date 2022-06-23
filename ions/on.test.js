8f8a94cb14ad4d82084eb4f916676ad176042701
Mike Lee <iskitz@users.noreply.github.com>
2022-06-23 08:19:09 -0700

    test: ~on: + single & multi-reaction & single, multi-term & for-all +
    also
    : refined all tests to match current test display style
    : refined re per spec

--- ions/on.test.js
+++ ions/on.test.js
@@ -2,79 +2,122 @@
 ~
 { re:
     { id:  'on.test@ionify'
-    , of:  'ionify.net'
-    , is:  'examination'
-    , by: ['mike.lee', 'team']
+    , of: ['tests']
+    , as: ['examination']
+    , by: ['mike.👨🏾‍💻.lee', 'team']
     , on:  -4.200709
-    , to:  -7.20201029
-    , at:  -0.1
-    , it: "tests ionify's ~on api"
-    }
-}
+    , to:  -7.20220623
+    , at:  -0.017
+    , is: "ionify's ~on api examination"
+}   }
 
 ~
-{ on
-:   'hi'
-,    hi
-: function onHi (ion)
-    { ~{log:"hi " + String (ion.hi) + "!"}
-    }
-}
+{ on:'fatherhood',
+      fatherhood :function
+      fatherhood (action)
+        { action.fatherhood === "かいととマイケル"
+        ? ~{  log:"✅ ~on: reaction: named & referenced"}
+        : ~{error:"🚫 ~on: reaction: named & referenced"}
+}       }
++
+{fatherhood:"かいととマイケル"}
+
 
 ~
-[ {hi:        "iamai"         }
-, {hi: "かいととひろことマイケル" }
-, {hi:      "['0 . 0']"       }
-]
+{ on:'ion@doma.in',
+     'ion@doma.in':function
+        on_ion_id (action)
+          {   action.re.id === 'ion@doma.in'
+          &&  action.re.is === "an re ionified module"
+          ?  ~{  log:"✅ ~on: reaction: named re.id"}
 
+          :   action['ion@doma.in'].is === "a top-level-id ionified module"
+          ?  ~{  log:"✅ ~on: reaction: named top-level-id"}
 
-~/... Test on:id ...................../
+          :  ~{error:"🚫 ~on: reaction: named top-level-id & re.id"}
+}         }
 +
-{ on
-:   'ion@doma.in'
-,   'ion@doma.in'
-: function onIonId (ion)
-    { ~{log: ["on:id:", ion.re.it, "with", ion.data, "that works!"]}
-    }
-}
-
-
-~/... with a standard re.id ion ....../
-+
-{re
-: { id: 'ion@doma.in'
-  , by: 'mike.lee@ionify'
+{re:
+  { id: 'ion@doma.in'
+  , by: 'mike.👨🏾‍💻.lee@ionify'
   , on: -8.20171111
-  , to: -8.20190101
+  , to: -7.20220623
   , in: 'san-jose.california.usa.earth'
-  , it: "is an re.id ion"
-  }
-, data:"some data"
-}
-
-
-~/... and a top-level-id ion ........./
+  , is: "an re ionified module"
+} }
 +
-{'ion@doma.in'
-:   { by: 'mike.lee@ionify'
-    , on: -8.20171111
-    , to: -8.20190101
-    , in: 'san-jose.california.usa.earth'
-    , it: "is a top-level-id ion"
-    }
-, data:"some other data"
-}
+{'ion@doma.in':
+  { by: 'mike.👨🏾‍💻.lee@ionify'
+  , on: -8.20171111
+  , to: -7.20220623
+  , in: 'san-jose.california.usa.earth'
+  , is: "a top-level-id ionified module"
+} }
 
+~
+{ on:'action',
+     'action':function
+    reaction1 (action)
+      { try
+          { ++action.count == 1
+            ? ~{  log:"✅ ~on: reaction: per sensation: once"}
+            : ~{error:"🚫 ~on: reaction: per sensation: once"}
+          }
+        catch (error)
+          {
+          ~ /~on    test try-catch prevents disrupting subsequent tests/
+          & /~error records the error message before throwing the error/
+          }
+}     }
+~
+{ on:'action',
+     'action':function
+    reaction2 (action)
+      { try
+          { ++action.count == 2
+            ? ~{  log:"✅ ~on: reaction: per sensation: multiple"}
+            : ~{error:"🚫 ~on: reaction: per sensation: multiple"}
+          }
+        catch (error)
+          {
+          ~ /~on    test try-catch prevents disrupting subsequent tests/
+          & /~error records the error message before throwing the error/
+          }
+}     }
+~
+{action:'activation',count:0}
 
-~/... Test ~on:id,do ................./
-+
+~
 { on:
-    [          'kana@ions.iskitz.net'
-    , ['wakatta.view@ions.iskitz.net', 'show']
-    , ['wakatta.game@ions.iskitz.net', 'start', 'stop', 'guess', 'next']
+    [ [ 'multi-termed', 'reaction']
+    ,  'single-termed----reaction'
+    ,        '*'
     ]
-, dont:
-    [ ""
-    ]
+    ,
+ '*':function
+  all_reactions (action)
+    { if (all_reactions == action.no) return  ~{log: "✅ ~on: reaction: all"}
+      if (all_reactions.happened    ) return; else all_reactions.happened = true
+    ~ {on:all_reactions.with.re.id,   no:all_reactions}
+    //~on:* keeps & maps reactions to their ids, not '*' <= 🐛 to fix?
+    //~on:* keep  & map  could  do {'*':[reaction,reaction,...]}
+    //~on:* would automatically do this if multi-reaction per sensation created
+    }
+    ,
+ 'single-termed----reaction':function
+  single_termed____reaction  ( action )
+    {
+    ~ {log: "✅ ~on: reaction: single-termed"}
+    }
+    ,
+ 'multi-termed reaction':function
+  multi_termed_reaction  ( action )
+    {
+      ~ {log: "✅ ~on: reaction: multi-termed"}
+    }
 }
++
+{'multi-termed':'action', reaction:true}
++
+{'single-termed----reaction':true}
 ;
