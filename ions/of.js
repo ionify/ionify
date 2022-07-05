f2a493c942c6041419df8f9bedba538dad1ca548
Mike Lee <iskitz@users.noreply.github.com>
2022-07-05 05:25:56 -0700

    docs: of@ fixed re.go.code+test links & tidied re.we notes

--- ions/of.js
+++ ions/of.js
@@ -7,8 +7,8 @@
     , as: ['aggregation','convention','sensation']
     , by: ['mike.lee', 'team']
     , on:  -4.200709
-    , to:  -7.20220704
-    , at:  -0.0
+    , to:  -7.20220705
+    , at:  -0.01
     , is:
         [ "ionify's re.of & of convention"
         ],
@@ -16,8 +16,8 @@
         { seek: 'https://api.ionify.net/'
         , deal: 'https://deal.ionify.net/'
         , help: 'https://github.com/ionify/ionify/issues'
-        , code: 'https://github.com/ionify/ionify/blob/public/ions/with.ion.js'
-        , test: 'https://github.com/ionify/ionify/blob/public/ions/with.ion.test.js'
+        , code: 'https://github.com/ionify/ionify/blob/public/ions/of.js'
+        , test: 'https://github.com/ionify/ionify/blob/public/ions/of.test.js'
         , plan: 'https://github.com/ionify/ionify/projects/1?fullscreen=true'
         , join: 'https://github.com/ionify/about/tree/public/team'
         },
@@ -31,19 +31,8 @@
         + "unless they alreay are. all spaces are name@domain where domain can "
         + "be blank."
 
-        , "want .with.ion on sion methods to access ion contexts e.g: domain"
-        , "want .with.ion.domain(s)+space(s)"
         , "want .with.ion.* to include the sion's re.of & sion.of spaces"
         , "want .with.ion to be exactly sion.of whether declared in sion or not"
-        , "want .with.ion to be accessible to only the sion it's populated for?"
-        , "know .with.ion can be exposed if shared via host method as reaction"
-
-        , "muse about re.it tags' & re.as descript*ions' purposes & differences"
-        , "note that *ion words may be consistent & reliable way of tagging "
-        + "ions. that set of words is well established & comprehensively "
-        + "descriptive & of course includes & completes with 'ion' 🤓"
-
-        , "like ion.with()'s existence being a sensible aggregation indication"
         ]
     },
 
