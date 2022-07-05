914dee220aa4ad10ad67bbedf9acd6cf55eedcf1
Mike Lee <iskitz@users.noreply.github.com>
2022-07-05 05:20:34 -0700

    code: as@ initial implementation

---
+++ ions/as.js
@@ -0,0 +1,64 @@
+;
+~
+{ re:
+    { id:  'as@ionify'
+    , im:  'as.im@ionify'
+    , of: ['core','sion','context','api']
+    , as: ['resolution','acquisition','convention','sensation']
+    , by: ['mike.lee', 'team']
+    , on:  -4.200709
+    , to:  -7.20220705
+    , at:  -0.0
+    , is:
+        [ "ionify's as convention"
+        ],
+      go:
+        { seek: 'https://api.ionify.net/'
+        , deal: 'https://deal.ionify.net/'
+        , help: 'https://github.com/ionify/ionify/issues'
+        , code: 'https://github.com/ionify/ionify/blob/public/ions/as.js'
+        , test: 'https://github.com/ionify/ionify/blob/public/ions/as.test.js'
+        , plan: 'https://github.com/ionify/ionify/projects/1?fullscreen=true'
+        , join: 'https://github.com/ionify/about/tree/public/team'
+        },
+      we:
+        [ "were implementing ionify's re.as convention"
+        , "must"
+        , "will"
+        , "plan"
+ 
+        , "muse about re.it tags' & re.as descript*ions' purposes & differences"
+        
+        , "note that *ion words may be a consistent & reliable way of tagging "
+        + "ions. that set of words is well established & comprehensively "
+        + "descriptive & of course includes & completes with 'ion' 🤓"
+        ]
+    },
+
+  on:'*',
+     '*':'as'
+        ,
+  of:
+    { own:
+        {
+        },
+      domain:
+        {
+        }
+    },
+
+  valueOf:function
+  ionify()
+    { this.re.as || (this.re.as = [])
+      this.as(this)
+      delete this.valueOf <- this
+      delete this['*']
+      delete this.as
+    },
+
+  as:function
+  as(obi)
+    { var as_  = obi.re.as
+    }
+}
+;  
