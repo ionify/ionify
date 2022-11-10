d2285d834c8c4f81ba81c22ed1ddcaa32cbb2a94
Mike Lee <iskitz@users.noreply.github.com>
2022-11-10 09:47:54 -0800

    code: re.id: faster & simpler initialization + re.do exploration
    and
    : re.as negative version@s indicating unstable incompatibility
    : re.we rearranged, refined, pruned & expanded
    : removed obsolete hip-hop invocation code
    : hip-hop invocation is handled-in-place handled-on-prototype
    invocation.
    
    re.id
    
    : indicates the identification@s & version@s of the actions &
    information id@ it adopts & applies.
    
    : ionify can automatically sense & ~get these during the ion's
    initialization.
    
    
    how
    :

--- ions/id.js
+++ ions/id.js
@@ -3,11 +3,12 @@
 { re:
     { id:  'id@ionify'
     , of: ['core','public','sion','api']
-    , as: {sion:0.001, convention:0.001, sensation:0.001}
-    , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike👨🏾‍💻lee', 'team✨ionify']
+    , as: {sion:-0.001, sensation:-0.001, convention:-0.001}
+    , do: {'next.id@ionify':true}
+    , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike.👨🏾‍💻.lee', 'team✨ionify']
     , on:  -4.200709
-    , to:  -8.20221107
-    , at:  -0.046
+    , to:  -8.20221110
+    , at:  -0.047
     , is:
         [ "ensuring that all ionified objects have an re.id."
         , "setting an object's re.id value as a member mapped to its object re."
@@ -23,22 +24,21 @@
         , join: 'https://github.com/ionify/about/tree/public/team'
         },
       we:
-        [ "plan to have top-level-ids reference entire ion not just its re & "
+        [ "have id.nextId's range as opposite next.id's to minimize overlap."
+
+        , "PLAN to have top-level-ids reference entire ion not just its re & "
         + "this requires re@, ~find, ~share & ~link's compatibility with that."
 
-        , "want id.domain string via id:{domain:'domain',toString:'id@domain'}"
-        , "have id.nextId's range as opposite next.id's to minimize overlap."
+        , "WILL rename re.id@ <= id@ to indicate collaboration with re@."
+        , "WANT id.domain string via id:{domain:'domain',toString:'id@domain'}"
+
         , "like ions with old ids being updated once next.id's available."
-        , "like linking this ion although it doesn't currently rely on it"
         , "like removing anions: anonymous ions' temporary .re.id & .re's? 🤔"
         ]
     },
 
   on:'*',
-     '*':'id'
-        ,
-
-  id:function
+ '*':function
   id( ion )
     { var ID = ion.re && ion.re.id
 
@@ -67,12 +67,6 @@
 
       ion [ID = ion.re.id]  ||  (ion [ID] = ion.re)
       return ID
-    },
-
-  valueOf:function
-  ionify ()
-    { ionify.with =  this.id.with = {its:this}
-    ; delete this.valueOf <- this & {get:'next.id@ionify'}
     }
 }
 ;
