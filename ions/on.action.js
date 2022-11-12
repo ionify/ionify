76a616d1edea563ee4efb5b29a806824867b1dcb
Mike Lee <iskitz@users.noreply.github.com>
2022-11-11 19:42:45 -0800

    code: on.action: enable invoked ion methods to evaluate in ion context
    and
    : re.by acknowledges :bowing_man:🏾‍♂️ יהוה 🤲🏾
    : re.by enhanced mike.:man:🏾‍:computer:.lee
    : re.by enhanced team:sparkles:ionify
    : re.of added
    : re.as converted per specification@
    : explored, investigated & noted how invoking an array's pop() doesn't
    function as expected. still pursuing understanding why...

--- ions/on.action.js
+++ ions/on.action.js
@@ -2,11 +2,13 @@
 ~
 { re:
     { id:  'on.action@ionify'
-    , as:  'sensor'
-    , by: ['mike.lee', 'team']
+    , of: ['core', 'public', 'observation', 'api']
+    , do: {transcription:-0.001}
+    , as: {observation  :-0.001}
+    , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike👨🏾‍💻lee', 'team✨ionify']
     , on:  -4.200709
-    , to:  -8.20221031
-    , at:  -0.1
+    , to:  -8.20221111
+    , at:  -0.023
     , is:
         [ 'ion, invoked object notation, interactions from your functions'
         , 'enabling more ions to have similar interaction interfaces'
@@ -24,6 +26,17 @@
       we:
         [ 'will create or identify any existing function id; read ECMA-262 [] spec.    '
         , 'need to fix ~[777].pop = ~[777].prototype.pop() = TypeError; this != [777]. '
+
+        + "   var xdf = {length:999, 998:'yoooooh'}; "
+        + "   ion.apply(xdf); 'yoooooh'; xdf.length == 998"
+
+        + "   var p7p = [777].pop;"
+        + "   p7p()"
+
+        + "   Uncaught TypeError: Cannot convert undefined or null to object"
+        + "     at pop (<anonymous>)"
+        + "     at <anonymous>:1:1"
+
         , 'like to sense objects without their own method then create own method with  '
         + '   .ion=object that calls its inherited method with this=ion.               '
         , 'like that ~Function + this.apply() can do interesting things like           '
@@ -39,17 +52,17 @@
         act (ion)
           {  ion || (ion = this)
 
+              // ionify this function: +.re +.with.* +.the.*
               Object.prototype.valueOf.apply (ion)
 
-              var our = act.with.our
-                , id  = this.re.id
-                , my  = act.with.our.space (id || '@')
-                , got = ion.apply (my, ion.in || ion)
+              var our   = act.with.our
+                , id    = this.re.id
+                , my    = this.with.its || act.with.our.space (id || '')
+                , got   = ion.apply (my, ion.in || ion)
+                , debug = {debug: ['~', id,':',ion.name || 'anion', '() =>', got]}
                 , debugging = !! (our.logging && our.logging.debug)
 
-              debugging
-                ? act.with.our.debug ({debug: ['~', id || ion.name || 'anonymous', '() : ', got]})
-                :           ~{debug: ['~', id || ion.name || 'anonymous', '() : ', got]}
+              debugging ? act.with.our.debug (debug) : ~debug
 
               return (ion.out = got)
           }
