2f07321264d3b69e1ec6b144f4ab91772c0fcdfe
Mike Lee <iskitz@users.noreply.github.com>
2022-11-11 23:34:30 -0800

    code: re.hi: initial [inactive] hi: hidden invocation implementation
    more
    : may benefit from dynamic:sparkles:self:sparkles:reactions
    : will revisit later, currently intriguing but lower priority

---
+++ ions/re.hi.js
@@ -0,0 +1,96 @@
+;
+~
+{ re:
+    { id:  're.hi'
+    , of: ['core', 'public', 're', 'sion', 'api']
+    , as: {observation: -0.001}
+    , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike.👨🏾‍💻.lee', 'team✨ionify']
+    , on:  -4.200709
+    , to:  -8.20221110
+    , at:  -0.001
+    , hi:  false || true || 'ionify' || "other method name"
+    , is:
+        [ "ionify's re.hi@: hidden invocation, "
+        +           "sion@: structured invoked object notation, "
+        +      "sensation@: sensing action: active ion."
+
+        , "indicating   an alternate sim: structured ion method for invocation."
+        , "implementing an alternate locally-defined ion method for invocation."
+        ],
+      go:
+        { seek: 'https://api.ionify.net/'
+        , deal: 'https://deal.ionify.net/'
+        , help: 'https://github.com/ionify/ionify/issues'
+        , code: 'https://github.com/ionify/ionify/blob/public/re.hi.js'
+        , test: 'https://github.com/ionify/ionify/blob/public/ions/re.hi.test.js'
+        , plan: 'https://github.com/ionify/ionify/projects/1?fullscreen=true'
+        , join: 'https://github.com/ionify/about/tree/public/team'
+        },
+      it:
+        [ "..."
+        ],
+      we:
+        [ "WERE doing it's initial implementation! Thank You 🙇🏾‍♂️ יהוה 🤲🏾! 👨🏾‍💻🤓"
+        , "WILL implement calling a sim implemented elsewhere, e.g. ~get.ing it"
+
+        , "must ..."
+        , "will ..."
+        , "plan ..."
+        , "need ..."
+        , "want ..."
+        , "like having conventions 1st auto-applied to themselves"
+        , "know ..."
+        , "care ..."
+        , "wish ..."
+        , "note ..."
+        , "have ..."
+        , "dont ..."
+        , "wont ..."
+        , "cant ..."
+        ]
+    },
+
+  on:
+    [ 'ionify'
+    , '*'
+    ],
+
+ '*':function
+  hi(ion)
+    { var method = (ion || (ion = this)) && ion.re.hi;
+      if (!method) return //ion is an obi: observable ion, so let it be so
+
+      typeof    method == 'boolean'   //ion wants to be a hi: hidden ion, so
+        ?   ion.ionify  ? (ion.valueOf = hi.yo()) //set its local invocation
+                        :  delete ion.valueOf     //disable ion  observation
+        : ((ion.valueOf =  ion[method]) && (ion.re.hi = true))
+          //ion has its own method for hi: hidden invocation☝🏾
+
+      delete ion.re.hi //remove ion's re.hi for no re-sensing
+    },
+
+  yo:function
+  yo()
+    { return function
+        hi()
+          { /beaming ✨ be accessibly marvelous 🌱 impossible:nothing ✨//
+            {ionify:this}/*|| delete this.valueOf <- this*/ &
+
+            /🙇🏾‍♂️🤲🏾 ¡godoit! 🤲🏾👨🏾‍💻//
+            this.ionify && this.ionify()
+          }
+    },
+
+  ionify:function
+  ionify  (action)
+    { var ion = action.re.ex.from.with.its
+      delete ion.valueOf <- ion
+    },
+
+  valueOf:function
+  ionify()
+    { this.hi.yo = yo
+      this.ionify  ()
+    }
+}
+;
