31bd89e712bf062b4b2c69432f16ec847d70284c
Mike Lee <iskitz@users.noreply.github.com>
2022-11-17 04:58:48 -0800

    code: re.id@ & .domain act as string & .domain's auto-set with .parts
    and
    : re.hi + inactive handled-invocation initialization
    : re.go: + read & team, updated join & set test before code
    : creates an re object with name & domain parts
    : re object & its domain object act as strings for convenience
    : + hip invocation to set temporary with.its
    : hip: handled-in-place
    
    next
    : explore re.hi:true + hop: handled-on-prototype invocation
    : auto-populating re.on.to.at.as.do from compound re.id

--- ions/id.js
+++ ions/id.js
@@ -7,8 +7,9 @@
     , do: {'next.id@ionify':true}
     , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike.👨🏾‍💻.lee', 'team✨ionify']
     , on:  -4.200709
-    , to:  -8.20221110
-    , at:  -0.047
+    , to:  -8.20221117
+    , at:  -0.048
+    , hi:  true
     , is:
         [ "ensuring that all ionified objects have an re.id."
         , "setting an object's re.id value as a member mapped to its object re."
@@ -16,39 +17,43 @@
         ],
       go:
         { seek: 'https://api.ionify.net/'
+        , read: 'https://lingo.ionify.net/'
         , deal: 'https://deal.ionify.net/'
         , help: 'https://github.com/ionify/ionify/issues'
-        , code: 'https://github.com/ionify/ionify/blob/public/ions/id.js'
         , test: 'https://github.com/ionify/ionify/blob/public/ions/id.test.js'
+        , code: 'https://github.com/ionify/ionify/blob/public/ions/id.js'
         , plan: 'https://github.com/ionify/ionify/projects/1?fullscreen=true'
-        , join: 'https://github.com/ionify/about/tree/public/team'
+        , join: 'https://join.ionify.net/'
+        , team: 'https://team.ionify.net/'
         },
       we:
-        [ "have id.nextId's range as opposite next.id's to minimize overlap."
+        [ "have id.nextId's range as opposite next.id's to delay their overlap."
+        , "MUST right ion[id] = ion.re logic enabling ~on:id@...,id@...:action."
+        , "WILL rename re.id@ <= id@ to indicate collaboration with re@."
+        , "want auto-populating re.on.to.at.as.do from compound re.id 🙇🏾‍♂️ה🤲🏾!"
 
         , "PLAN to have top-level-ids reference entire ion not just its re & "
         + "this requires re@, ~find, ~share & ~link's compatibility with that."
-
-        , "WILL rename re.id@ <= id@ to indicate collaboration with re@."
-        , "WANT id.domain string via id:{domain:'domain',toString:'id@domain'}"
 
         , "like ions with old ids being updated once next.id's available."
         , "like removing anions: anonymous ions' temporary .re.id & .re's? 🤔"
         ]
     },
 
-  on:'*',
+  on:
+ '*',
  '*':function
   id( ion )
-    { var ID = ion.re && ion.re.id
+    { var sense = id.with.its
+        ,    ID = ion.re && ion.re.id
 
       if(ID || isFinite (ID))
-          return ion [ID]
-            ?         ID
-            :   (ion [ID] = ion.re).id
-            ;
+        { ion [ID] || (ion [ID] = ion.re)
+          return sense.d0main (ion)
+        }
 
-      if (ID = ion.re.id) return ID
+
+      if (ID = ion.re.id) return sense.d0main (ion)
 
       var named
         = (ion.debug ||  ion.log || ion.error || ion.info || ion.warn)
@@ -65,8 +70,41 @@
          /*|*/                                    || 9007199254740991           //
            )/*--------*/)/*-----------*/)/*-------------------------------------*/
 
-      ion [ID = ion.re.id]  ||  (ion [ID] = ion.re)
-      return ID
+      ion [ID = ion.re.id] || (ion [ID] = ion.re)
+      return sense.d0main (ion)
+    },
+
+  string :  function
+  string () {return this.name}
+    ,
+  domainParser
+    : /@(.+)\.(.+\..+)$|@(.+\..+)$|@(.*)$|^[^@]+$/
+    ,
+  d0main :function
+  domain (ion)
+    { var sense       = (this.d0main && domain === this.d0main)
+                      ?  this
+                      :  domain.with.its
+        , id          = String (ion.re.id)
+        , dOmain      = {}
+        ; found       = id.match (sense.domainParser)
+        ; dOmain.full = found [0] // parse the ion's domain name
+        ; dOmain.name = found [4] || ''
+        ; dOmain.subs = found [1] || ''
+        ; dOmain.base = found [2] || found [3]
+        ; ion.re.id   = id  = {name:id, domain:dOmain/*, __proto__:String.prototype*/}
+        ; id.valueOf  =
+          id.toString =
+          id.domain.valueOf  =
+          id.domain.toString = sense.string
+      Object.setPrototypeOf (id, String.prototype)
+      return id
+    },
+
+  valueOf:function
+  ionify ()
+    { this["*"].with = {its:this}
+      delete this.valueOf <- this
     }
 }
 ;
