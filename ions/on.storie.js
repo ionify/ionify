ca47520b22bdf0259f84b11c43b88416fe7fd073
Mike Lee <iskitz@users.noreply.github.com>
2022-12-05 08:32:18 -0800

    code & docs: storie@: re.do transcription & re.by mike:guyana::man:🏾‍:computer::us:lee
    and
    : re.go standardized meet, seek, deal, help, test, code, plan, join &
    team hyperlinks

--- ions/on.storie.js
+++ ions/on.storie.js
@@ -3,24 +3,25 @@
 { re:
     { id:  'on.storie@ionify'
     , of: ['core','public','conduit','api']
-    , as: {observation:-0.001}
-    , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike.👨🏾‍💻.lee', 'team✨ionify']
+    , do: {transcription:-0.001}
+    , as: {observation  :-0.001}
+    , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike.🇬🇾👨🏾‍💻🇺🇸.lee', 'team✨ionify']
     , on:  -8.20161116
-    , to:  -8.20221117
-    , at:  -0.007
+    , to:  -8.20221205
+    , at:  -0.008
     , is:
         [" sensing activated stories: string template or regular ion expressions like "
         ,"              ~/.../                                                       "
         ," currently transcribing storie content.                                            "
         ],
       go:
-        { seek: 'https://api.ionify.net/'
-        , read: 'https://lingo.ionify.net/'
+        { meet: 'https://meet.ionify.net/'
+        , seek: 'https://seek.ionify.net/'
         , deal: 'https://deal.ionify.net/'
-        , help: 'https://github.com/ionify/ionify/issues'
+        , help: 'https://help.ionify.net/'
       //, test: 'https://github.com/ionify/ionify/blob/public/ions/on.storie.test.js'
         , code: 'https://github.com/ionify/ionify/blob/public/ions/on.storie.js'
-        , plan: 'https://github.com/ionify/ionify/projects/1?fullscreen=true'
+        , plan: 'https://plan.ionify.net/'
         , join: 'https://join.ionify.net/'
         , team: 'https://team.ionify.net/'
         },
