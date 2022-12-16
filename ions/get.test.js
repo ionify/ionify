2d9a515c7302ec3c7472cb9719d10888d812d06b
Mike Lee <iskitz@users.noreply.github.com>
2022-12-16 07:28:30 -0800

    tests: get.test@ ~hop:do <= ~hip & reformatted test messages ++reading
    and
    : re.by: mike.:guyana::man:🏾‍:computer::us:.lee
    : re.on+to hebrew & gregorian calendared & timezoned instantiation@s
    : re.go: + read + talk hyperlinks
    : .with.my <= .with.its
    : invokes transcription before test-result's extra code
    : separated invocations where easy to read for ++break~pointing &
    comprehension

--- ions/get.test.js
+++ ions/get.test.js
@@ -5,18 +5,20 @@
     , of: ['tests','core','api']
     , do: {acquisition:true, transcription:true}
     , as: {examination:-0.001}
-    , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike.👨🏾‍💻.lee', 'team✨ionify']
-    , on:  -4.200709
-    , to:  -8.20221201
-    , at:  -0.023
+    , by: ['🙇🏾‍♂️ יהוה 🤲🏾', 'mike.🇬🇾👨🏾‍💻🇺🇸.lee', 'team✨ionify']
+    , on:  -24.200709
+    , to:  -18.578309226
+    , at:  - 0.025
     , is:  "ionify's ~get api examination"
     , go:
         { meet: 'https://meet.ionify.net/'
         , seek: 'https://seek.ionify.net/'
         , deal: 'https://deal.ionify.net/'
+        , read: 'https://read.ionify.net/'
         , help: 'https://help.ionify.net/'
         , test: 'https://github.com/ionify/ionify/blob/public/ions/get.test.js'
         , code: 'https://github.com/ionify/ionify/blob/public/ions/get.js'
+        , talk: 'https://talk.ionify.net/'
         , plan: 'https://plan.ionify.net/'
         , join: 'https://join.ionify.net/'
         , team: 'https://team.ionify.net/'
@@ -30,11 +32,9 @@
         ]
     },
 
-  valueOf:function
-  test_get()
-    { delete this.valueOf <= this
-
-      var state = +{info:null}
+  do:function
+  test_get ()
+    { var state = +{info:null}
 
       ~ {get:''}         <=  /testing getting the .js file/
       ~ {get:'@ionify'}  <=  /testing getting the @domain's .js file/
@@ -42,82 +42,81 @@
       ~
       { get:'narrator@ionify'
       , and:
-          { log: "✅ ~get:script and:{} do oli: object-literal invocation"
+          { log: "✅ ~get:script and:do {} oli: object-literal invocation"
           }
       }
-      +
+      ~
       { get:'narrator@ionify'
       , and:
-          [{ log: "✅ ~get:script and:[{}] do aeon: array-expressed object notation"
+          [{ log: "✅ ~get:script and:do [{}] aeon: array-expressed object notation"
           }]
       }
-      +
+      ~
       { get:'narrator@ionify'
       , and:
           [ {info:true}
-          , /✅ ~get:script and:[\/\/,{}] do aeon: array-expressed object notation/
+          , /✅ ~get:script and:do [\/\/,{}] aeon: array-expressed object notation/
           , {info:state}
           ]
       }
-      +
+      ~
       { get:'narrator@ionify'
       , and:
-          /✅ ~get:script and: \/\/     do storie: stated text of recognition expression/
+          /✅ ~get:script and:do  \/\/     storie: stated text of recognition expression/
       , info:true
       }
-      +
+      ~
       { get:'narrator@ionify'
       , and:function
         and ()
           {
-          + /✅ ~get:script and:Function/
+          + { log: ["✅ ~get:script and:do Function", and.with.my.get]}
           }
       }
-      +
+      ~
       {info:state}
 
       ~
       { get : 'narrator@ionify'
       , in  : 'order'
       , then:
-          { log: "✅     ~get:script     in:order then:{} do oli: object-literal invocation"
+          { log: "✅     ~get:script     in:order then:do {} oli: object-literal invocation"
           }
       , warn: "🚨 need invocations returning invoked-or-not # state to confirm 🚨"
       }
-      +
+      ~
       { get : 'narrator@ionify'
       , in  : 'order'
       , then: function
         hi()
           {
-          + {log: ["✅     ~get:script    in:order then:Function", hi.with.its.get]}
+          + {log: ["✅     ~get:script    in:order then:do Function:", hi.with.my.get]}
           }
       }
 
-      +
+      ~
       { log:"testing ~get grouped & ordered with a shared ~get.then..."
-      , say:"    ~get:scripts[] in:order then:Function"
+      , say:"    ~get:scripts[] in:order then:do Function"
       , err:"🚨 must make get.batch@web@ .onload that ~.thens after all load 🚨"
       ,  in:'order'
       , get:
           [ 'https://cdn.jsdelivr.net/gh/iskitz/ajile/use/com.iskitz.ajile.js?mvcoff,mvcshareoff'
           , 'https://cdn.jsdelivr.net/gh/iskitz/ajile/play/api/scripts/com.iskitz.ajile.examples.LoadExample.js'
           ],
-        then        : function
+        then:         function
         onLoadExample (script)
           { var got = '\n   ' + (script.target || script.re).id
-              , its = onLoadExample.with.its
-              , say = its.say
+              , my  = onLoadExample.with.my
+              , say = my.say
 
             com.iskitz.ajile.examples
-              ? {log  : ["✅", say, got]}
-              +  com.iskitz.ajile.examples.LoadExample
-              : {error: ["❌", say, got], halt:false}
-              + {warn : its.err}
+              ? {log  : ["✅", say, got]} + com.iskitz.ajile.examples.LoadExample
+              : {error: ["❌", say, got]  , halt:false}
+              + {warn : my.err}
             }
       }
 
-      +
+      ~
       { log: "testing ~get's individually ~do-group ordered with 1 aliased & shared then"
       , do:
           [ {  get: 'https://ajile.net/use/com.iskitz.ajile.js?mvcoff,mvcshareoff'
@@ -131,8 +130,8 @@
         doIt:function
         onIncludeExample (script)
           {  var got = '\n   ' + (script.target || script.re).id
-             com.iskitz.ajile.examples.IncludeExample
-          + {log: ["✅ ~do:[get:script    in:order then:Function:named]", got]}
+          ~ {log: ["✅ ~do:[get:script    in:order then:Function:named]", got]}
+          ~  com.iskitz.ajile.examples.IncludeExample
           }
       }
 
